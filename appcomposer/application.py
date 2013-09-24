bf6970442b63c120587a4dc4393687b6a2d8a69a
Luis Rodriguez <luis.rodriguezgil@deusto.es>
2013-09-24 16:53:23 +0200

    [Users]: Added basic auth (which will probably need major refactoring, and to be moved out of the main application file)

--- appcomposer/application.py
+++ appcomposer/application.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template, render_template_string
 import os
 
 app = Flask(__name__)
@@ -14,11 +15,11 @@ app.config["SECRET_KEY"] = os.urandom(32)
 from .user.user_application import UserApplication
 from .admin import admin_blueprint
 
-
 # User component
 userApp = UserApplication(app)
 
 app.register_blueprint(admin_blueprint,     url_prefix = '/admin')
+
 
 #####
 # Composers
@@ -31,4 +32,45 @@ from .composers.expert    import expert_blueprint
 app.register_blueprint(translate_blueprint, url_prefix = '/composers/translate')
 app.register_blueprint(adapt_blueprint,     url_prefix = '/composers/adapt')
 app.register_blueprint(expert_blueprint,    url_prefix = '/composers/expert')
+
+
+
+
+# This should be moved somewhere else. Meanwhile, I place it here for testing/development purposes.
+from flask.ext.wtf import TextField, Form, PasswordField, NumberRange, DateTimeField
+from flask import request, redirect, url_for, session
+import db
+import models
+
+class LoginForm(Form):
+    login               = TextField(u"Login:")
+    password            = PasswordField(u"Password:")
+
+@app.route('/login', methods = ["GET", "POST"])
+def login():
+    
+    if len(request.form):
+        # We probably got here from a POST
+        form = LoginForm(request.form, csrf_enabled = True)
+    else:
+        # We probably got here through a GET
+        form = LoginForm()
+        form.login.data = "TestLogin"
+        form.password.password = "TestPassword"
+        
+    # This is an effective login request
+    if request.method == "POST":
+        num = db.db_session.query(models.User).filter_by(login = form.login.data, auth_data = form.password.data).count()
+        if(num == 0):
+            return "Wrong user or password"
+        else:
+            # Store the username in the session object.
+            # The session is stored client-side but cryptographically signed.
+            session["username"] = form.login.data
+            return redirect("/user")
+        
+        
+    return render_template("login/login.html", form=form)
+
+
 
