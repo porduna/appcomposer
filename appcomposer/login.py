31de9261ba6f183b14a82a1ed50151d40a3a2c56
Pablo Orduña <pablo.orduna@deusto.es>
2013-10-08 16:15:08 +0200

    Add new login form

--- appcomposer/login.py
+++ appcomposer/login.py
@@ -1,6 +1,6 @@
-from flask import session, render_template, render_template_string
+from flask import session, render_template, render_template_string, flash
 
-from flask.ext.wtf import TextField, Form, PasswordField, NumberRange, DateTimeField
+from flask.ext.wtf import TextField, Form, PasswordField, NumberRange, DateTimeField, validators
 from flask import request, redirect, url_for, session
 
 from .db import db_session
@@ -15,35 +15,26 @@ def current_user():
     return db_session.query(User).filter_by(login = session['login']).first()
     
 class LoginForm(Form):
-    login               = TextField(u"Login:")
-    password            = PasswordField(u"Password:")
+    login               = TextField(u"Login:", validators = [ validators.Required() ])
+    password            = PasswordField(u"Password:", validators = [ validators.Required() ])
 
 @app.route('/login', methods = ["GET", "POST"])
 def login():
-
     next_url = request.args.get('next', '')
     
-    if len(request.form):
-        # We probably got here from a POST
-        form = LoginForm(request.form, csrf_enabled = True)
-    else:
-        # We probably got here through a GET
-        form = LoginForm()
-        form.login.data = ""
-        form.password.data = "TestPassword"
+    form = LoginForm(request.form)
         
     # This is an effective login request
-    if request.method == "POST":
+    if form.validate_on_submit():
         num = db_session.query(User).filter_by(login = form.login.data, auth_data = form.password.data).count()
-        if(num == 0):
-            return "Wrong user or password"
+        if num == 0:
+            flash("Invalid login")
         else:
             # Store the username in the session object.
             # The session is stored client-side but cryptographically signed.
             session["logged_in"] = True
             session["login"] = form.login.data
             return redirect(next_url or "/user")
-        
         
     return render_template("login/login.html", form=form, next=next_url)
 
