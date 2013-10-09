699d9156d7d71f83af583d30dff6442bb206586a
Luis Rodriguez <luis.rodriguezgil@deusto.es>
2013-10-09 15:13:00 +0200

    Added .idea to gitignore, fixed minor bug in App model code, continued AppStorage work

--- appcomposer/appstorage/appstorage.py
+++ appcomposer/appstorage/appstorage.py
@@ -5,10 +5,48 @@ from flask import session, render_template, render_template_string
 from flask.ext.wtf import TextField, Form, PasswordField, NumberRange, DateTimeField
 from flask import request, redirect, url_for, session
 
+from appcomposer.login import current_user
 from appcomposer.db import db_session
-
 from appcomposer.application import app
+from appcomposer.models import App
+
+import random
+
+import json
 
 @app.route('/appstorage', methods = ["GET", "POST"])
 def appstorage():
     return "Hello appstorage"
+
+@app.route('/appstorage/new', methods=["GET","POST"])
+def new():
+    name = request.args.get("name")
+    owner = current_user()
+    result = create_app(name, owner, "dummy", "{'message':'Hello world'}")
+    return "Application created"
+
+
+
+def create_app(name, owner, composer, data):
+    """
+    create_app(name, data)
+    @param name Unique name to give to the application.
+    @param owner Owner login.
+    @param composer Composer identifier.
+    @param data JSON-able dictionary with the composer-specific data.
+    """
+    
+    data = {
+            "version" : 1,
+            "composer" : composer,
+            "data" : data
+            }
+
+    appv = App(name, owner)
+    appv.data = json.dumps(data)
+
+    # Insert the new app into the database
+    db_session.add(appv)
+    db_session.commit()
+
+    return True
--- appcomposer/models.py
+++ appcomposer/models.py
@@ -8,25 +8,27 @@ from sqlalchemy.orm import relation, backref
 
 from appcomposer.db import Base, db_session as DBS
 
+
 class User(Base, UserMixin):
     __tablename__ = 'Users'
 
     id = Column(Integer, primary_key=True)
 
-    login               = Column(Unicode(50), unique = True )
-    name                = Column(Unicode(50), nullable = False)
-    password            = Column(Unicode(50), nullable = False) # hash
-    email               = Column(Unicode(254), nullable = False)
-    organization        = Column(Unicode(50))
-    role                = Column(Unicode(50))
-    creation_date       = Column(DateTime, nullable = False, index = True)
-    last_access_date    = Column(DateTime, nullable = False, index = True)
-    auth_system         = Column(Unicode(20), nullable = True)
-    auth_data           = Column(Unicode(255), nullable = True)
+    login = Column(Unicode(50), unique=True)
+    name = Column(Unicode(50), nullable=False)
+    password = Column(Unicode(50), nullable=False) # hash
+    email = Column(Unicode(254), nullable=False)
+    organization = Column(Unicode(50))
+    role = Column(Unicode(50))
+    creation_date = Column(DateTime, nullable=False, index=True)
+    last_access_date = Column(DateTime, nullable=False, index=True)
+    auth_system = Column(Unicode(20), nullable=True)
+    auth_data = Column(Unicode(255), nullable=True)
 
-    def __init__(self, login = None, name = None, password = None, email = None, organization = None, role = None, creation_date = None, last_access_date = None, auth_system = None, auth_data = None):
-        self.login    = login
-        self.name     = name
+    def __init__(self, login=None, name=None, password=None, email=None, organization=None, role=None,
+                 creation_date=None, last_access_date=None, auth_system=None, auth_data=None):
+        self.login = login
+        self.name = name
         self.password = password
         self.email = email
         self.organization = organization
@@ -37,7 +39,9 @@ class User(Base, UserMixin):
         self.auth_data = auth_data
 
     def __repr__(self):
-        return "User(%r, %r, %r, %r, %r, %r, %r, %r, %r, %r)" % (self.login, self.name, self.password, self.email, self.organization, self.role, self.creation_date, self.last_access_date, self.auth_system, self.auth_data)
+        return "User(%r, %r, %r, %r, %r, %r, %r, %r, %r, %r)" % (
+            self.login, self.name, self.password, self.email, self.organization, self.role, self.creation_date,
+            self.last_access_date, self.auth_system, self.auth_data)
 
     def __unicode__(self):
         return self.name
@@ -45,51 +49,50 @@ class User(Base, UserMixin):
     @classmethod
     def exists(self, login, word):
         return DBS.query(self).filter(sql.and_(self.login == login, self.password == word)).first()
-    
+
+
 class AppVersion(Base):
-    
     __tablename__ = 'AppVersions'
-    
-    version_id = Column(Integer, primary_key = True)
-    app_id = Column(Integer, ForeignKey("Apps.id"), primary_key = True)
-    creation_date = Column(DateTime, nullable = False, index = True)
-    
+
+    version_id = Column(Integer, primary_key=True)
+    app_id = Column(Integer, ForeignKey("Apps.id"), primary_key=True)
+    creation_date = Column(DateTime, nullable=False, index=True)
+
     app = relation("App", backref="app_versions")
-    
+
     def __init__(self, version_id, app):
         self.version_id = version_id
         self.app = app
         self.creation_date = datetime.datetime.now()
-    
+
 
 class App(Base):
-
     __tablename__ = 'Apps'
     __table_args__ = (UniqueConstraint('name', 'owner_id'), )
 
-    id = Column(Integer, primary_key = True)
+    id = Column(Integer, primary_key=True)
 
-    unique_id         = Column(Unicode(50), index = True, unique = True)
-    name              = Column(Unicode(50), index = True)
-    owner_id          = Column(Integer, ForeignKey("Users.id"), nullable = False, index = True)
-    composer          = Column(Unicode(50), index = True, nullable = False, server_default = u'expert')
-    data              = Column(Text, nullable = False, server_default = u'{}')
-    creation_date     = Column(DateTime, nullable = False, index = True)
-    modification_date = Column(DateTime, nullable = False, index = True)
-    last_access_date  = Column(DateTime, nullable = False, index = True)
+    unique_id = Column(Unicode(50), index=True, unique=True)
+    name = Column(Unicode(50), index=True)
+    owner_id = Column(Integer, ForeignKey("Users.id"), nullable=False, index=True)
+    composer = Column(Unicode(50), index=True, nullable=False, server_default=u'expert')
+    data = Column(Text, nullable=False, server_default=u'{}')
+    creation_date = Column(DateTime, nullable=False, index=True)
+    modification_date = Column(DateTime, nullable=False, index=True)
+    last_access_date = Column(DateTime, nullable=False, index=True)
 
     owner = relation("User", backref=backref("own_apps", order_by=id, cascade='all,delete'))
 
     def __init__(self, name, owner):
-        self.name  = name
+        self.name = name
         self.owner = owner
         self.creation_date = self.modification_date = self.last_access_date = datetime.datetime.now()
 
         self.unique_id = str(uuid.uuid4())
         while App.find_by_unique_id(self.unique_id) is not None:
             self.unique_id = str(uuid.uuid4())
-        
+
     @classmethod
-    def find_by_unique_id(self, unique_id):
-        return DBS.query(self).filter(unique_id = unique_id).first()
+    def find_by_unique_id(cls, unique_id):
+        return DBS.query(cls).filter_by(unique_id=unique_id).first()
 
