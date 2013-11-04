8a5493a906c1d12a7d213cc99f240666c137052f
Antonio Robles <arobles@scc.uned.es>
2013-11-04 10:49:41 +0100

    [Admin] The Admin can show the users registered in the platform (#52)

--- appcomposer/admin/admin_application.py
+++ appcomposer/admin/admin_application.py
@@ -1,8 +1,19 @@
 from flask import redirect, request, flash, session, render_template_string, url_for
 from flask.ext.admin import Admin, BaseView, AdminIndexView, expose
+from flask.ext.admin.contrib.sqlamodel import ModelView
 
+from flask.ext import wtf
+from wtforms.fields import PasswordField
+
+from appcomposer import models
 from appcomposer.login import current_user
+from appcomposer.db import db_session
 
+
+##########################################################
+#
+# Initialization
+# 
 
 def initialize_admin_component(app):
     # Initialize the Admin
@@ -10,38 +21,95 @@ def initialize_admin_component(app):
     # Endpoint enables us to do url_for('userp') to yield the URL
     url = '/admin'
     admin = Admin(index_view = AdminView(url = url, endpoint = 'admin'), name='Admin Profile', endpoint = "home-admin")
-    admin.add_view(UsersView(name='Users', url = 'users', endpoint = 'admin.users'))
+    admin.add_view(UsersView(db_session, name='Users', url = 'users', endpoint = 'admin.users'))
     admin.add_view(ApplicationsView(name='Applications', url = 'applications', endpoint = 'admin.applications'))    
     admin.add_view(ProfileView(name='My Profile', url = 'profile', endpoint = 'admin.profile'))    
     admin.init_app(app)
 
 
+#####################################################################
+#
+#
+# Parent views
+#
+# 
 
-class AdminBaseView(BaseView):
+class MyAdminIndexView(AdminIndexView):
 
     def is_accessible(self):
-        return current_user() is not None
+        self._current_user = current_user()
+        return self._current_user is not None
 
     def _handle_view(self, *args, **kwargs):
         if not self.is_accessible():
             return redirect(url_for('login', next=request.url))
+        
+        # Just call parent class with predefined model
+        return super(MyAdminIndexView, self)._handle_view(*args, **kwargs)
 
+
+class AdminBaseView(BaseView):
+
+    def is_accessible(self):
+        self._current_user = current_user()
+        return self._current_user is not None
+
+    def _handle_view(self, *args, **kwargs):
+        if not self.is_accessible():
+            return redirect(url_for('login', next=request.url))
+        
+        # Just call parent class with predefined model
         return super(AdminBaseView, self)._handle_view(*args, **kwargs)
 
 
-class AdminView(AdminBaseView):
+class AdminModelView(ModelView):
+
+    def is_accessible(self):
+        self._current_user = current_user()
+        return self._current_user is not None
+
+    def _handle_view(self, *args, **kwargs):
+        if not self.is_accessible():
+            return redirect(url_for('login', next=request.url))
+        
+        # Just call parent class with predefined model
+        return super(AdminModelView, self)._handle_view(*args, **kwargs)
+
+
+##############################################################
+#
+# Main views
+# 
+
+class AdminView(MyAdminIndexView):
     
     @expose('/')
     def index(self):       
         return self.render('admin/index.html')
 
 
-class UsersView(AdminBaseView):
+class UsersView(AdminModelView):
     
-    @expose('/')
-    def index(self):       
-        return self.render('admin/users.html')
+    column_list = ('login', 'name', 'email', 'organization', 'role')
 
+    def __init__(self, session, **kwargs):
+        super(UsersView, self).__init__(models.User, session, **kwargs)
+
+    column_labels = dict(login = 'Login', name = 'Full Name', email = 'E-mail', organization = 'Organization', role = 'Role')
+    column_filters = ('login', 'name', 'email', 'organization', 'role')
+
+    # List of columns that can be sorted
+    column_sortable_list = ('login', 'name', 'email', 'organization', 'role')
+  
+    # Columns that can used for searches
+    column_searchable_list = ('login', 'name', 'email', 'organization', 'role')
+       
+    # Fields used for the creations of new users    
+    #form_columns = ('login', 'name', 'email', 'organization', 'role')
+    #form_overrides = dict(access_level=wtf.SelectField, password=PasswordField)
+
+    #def on_model_change(self, form, model):
+    #    model.password = new_hash("sha", model.password).hexdigest()
 
 class ApplicationsView(AdminBaseView):
     
