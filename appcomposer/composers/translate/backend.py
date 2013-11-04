30d00de258acad39d5479e2d6fa816f1922f74d1
Luis Rodriguez <luis.rodriguezgil@deusto.es>
2013-11-04 14:37:33 +0100

    [translate]: Started work on BundleManager class. Can now parse simple standard Gadget specs and load the bundles.

--- appcomposer/composers/translate/backend.py
+++ appcomposer/composers/translate/backend.py
@@ -1,8 +1,55 @@
 import json
+import urllib
 from markupsafe import Markup
 from appcomposer.composers.translate import translate_blueprint
 from xml.dom import minidom
 import StringIO
+
+
+class BundleManager(object):
+    def __init__(self):
+        self._bundles = {}
+
+    def _read_url(self, url):
+        handle = urllib.urlopen(url)
+        contents = handle.read()
+        return contents
+
+    def load_spec(self, url):
+        xml_str = self._read_url(url)
+        locales = self._extract_locales(xml_str)
+        for loc in locales:
+            bundle_xml = self._read_url(loc[2])
+            bundle = Bundle.from_xml(bundle_xml, loc[0], loc[1])
+            name = self.get_name(loc[0], loc[1])
+            self._bundles[name] = bundle
+
+    def get_name(self, lang, country):
+        if lang is None or lang == "":
+            lang = "ANY"
+        if country is None or country == "":
+            country = "ANY"
+        return "%s_%s" % (lang, country)
+
+    def _extract_locales(self, xml_str):
+        locales = []
+        xmldoc = minidom.parseString(xml_str)
+        itemlist = xmldoc.getElementsByTagName("Locale")
+        for elem in itemlist:
+            messages_file = elem.attributes["messages"].nodeValue
+
+            try:
+                lang = elem.attributes["lang"].nodeValue
+            except KeyError:
+                lang = ""
+
+            try:
+                country = elem.attributes["country"].nodeValue
+            except KeyError:
+                country = ""
+
+            locales.append((lang, country, messages_file))
+        return locales
 
 
 class Bundle(object):
@@ -20,6 +67,10 @@ class Bundle(object):
 
     def remove_msg(self, word):
         del self._msgs[word]
+
+    def to_jsonable(self):
+        bundle_data = {"country": self.country, "lang": self.lang, "group": self.group, "messages": self._msgs}
+        return bundle_data
 
     def to_json(self):
         bundle_data = {"country": self.country, "lang": self.lang, "group": self.group, "messages": self._msgs}
@@ -69,12 +120,20 @@ def backend():
     """
 
     bundle = Bundle.from_xml(testxml, "es", "ES")
-
     jsonstr = bundle.to_json()
-
     bundle = Bundle.from_json(jsonstr)
-
     xmlstr = bundle.to_xml()
 
     return Markup.escape(xmlstr)
+
+
+@translate_blueprint.route('/backendt', methods=['GET', 'POST'])
+def backendt():
+    bm = BundleManager()
+    bm.load_spec("https://dl.dropboxusercontent.com/u/6424137/i18n.xml")
+
+    bundles = "";
+    for bundle in bm._bundles.values():
+        bundles += bundle.to_json()
+    return Markup.escape(bundles)
 
