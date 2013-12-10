6b36bb09cee0a3de18218207dd78efb7345cb136
Pablo Orduña <pablo.orduna@deusto.es>
2013-12-10 19:22:01 +0100

    Add first Graasp widgets. Still pending the verification of the Graasp service.

--- appcomposer/login.py
+++ appcomposer/login.py
@@ -1,3 +1,6 @@
+import json
+import urllib
+
 from flask import session, render_template, render_template_string, flash
 
 from flask.ext.wtf import TextField, Form, PasswordField, NumberRange, DateTimeField, validators
@@ -53,5 +56,42 @@ def logout():
 
 @app.route('/graasp-login')
 def graasp_login():
-    return "Working on it"
+    origin_url = url_for('graasp_login', _external = True)
+    widget_url = url_for('graasp_widget', _external = True)
+
+    data = {
+        "url": origin_url,
+        "phases": [
+            {
+                "items": [
+                    {
+                        "url": widget_url,
+                        "name": "Login app",
+                        "description": "Login app for App composer"
+                    },
+                ],
+                "name": "Login",
+                "description": "You may log in the app composer from here."
+            }
+        ],
+        "name": "App Composer log in space",
+        "description": "<p>In this space, you may log in the app composer automatically</p>\n"
+    }
+
+    url_base = "https://graasp.epfl.ch/spaces/instantiate/ils.json?ils="
+
+    space_creation_link = url_base + urllib.quote(json.dumps(data))
+    return render_template('login/graasp.html', space_creation_link = space_creation_link)
+
+@app.route('/graasp/authn/')
+def graasp_authn():
+    security_token = request.args.get('st', '')
+    # Step 1: do something with security token (e.g. check who is)
+    # Step 2: check if the user is in the database. If it is, log him in.
+    # Step 3: if he is not in the database, create the user. And then log him in.
+    return ":-)"
+
+@app.route('/login-widget.xml')
+def graasp_widget():
+    return render_template('login/widget.xml')
 
