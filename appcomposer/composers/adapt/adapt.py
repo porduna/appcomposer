be3f8bf551c54acbcef066c7faccfd79b8c43467
mllatg <mlatorre@ieec.uned.es>
2013-12-16 17:50:55 +0100

    [adaptor] Hypothesis tool works now

---
+++ appcomposer/composers/adapt/adapt.py
@@ -0,0 +1,3 @@
+from .edt import data as edt_data
+
+register_adapt(edt_data)
--- appcomposer/composers/adapt/edt.py
+++ appcomposer/composers/adapt/edt.py
@@ -1,5 +1,15 @@
 data = {
-    'new' : edt_new, # pointer to function
-    'load' : edt_load, # pointer to function
-    'id' : 'edt', # so when loading an application, we know which handle to use
+    'new' : 'edt_new', # pointer to function
+    'load' : 'edt_load', # pointer to function
+    'id' : 'edt', # so when loading an application, we know which handle to use, EQUIVALE AL TIPO QUE HABIA DEFINIDO
 }
+
+
+def edt_new():
+    # ...
+    x = 5
+    return x    
+
+def edt_load():
+    # ...
+    return "load"    
