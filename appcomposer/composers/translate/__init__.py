26374134b96d947185109e45709e9be2a2e16d8b
Luis Rodriguez <luis.rodriguezgil@deusto.es>
2013-12-18 13:16:15 +0100

    [translate]: EditLang screen now shows a pretty Publish button, which leads to a Publish dialog with a link to the translation for the chosen target group

--- appcomposer/composers/translate/__init__.py
+++ appcomposer/composers/translate/__init__.py
@@ -420,6 +420,24 @@ def translate_about():
     return render_template("composers/translate/about.html")
 
 
+@translate_blueprint.route("/publish")
+def translate_publish():
+    """
+    Show in a somewhat pretty way a link to an XML for a specific translation.
+    """
+    appid = request.values.get("appid")
+    if appid is None:
+        return render_template("composers/errors.html", message="appid not provided"), 500
+
+    group = request.values.get("group")
+    if group is None:
+        return render_template("composers/errors.html", message="group not provided"), 500
+
+    link = url_for('.app_xml', appid=appid, group=group, _external=True)
+
+    return render_template("composers/translate/publish.html", link=link)
+
+
 @translate_blueprint.route("/proposed_list", methods=["POST", "GET"])
 def translate_proposed_list():
     """
