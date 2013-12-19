f529c5ceb969f61e3e5a066466aa6af8bbc7ece5
Pablo Orduña <pablo.orduna@deusto.es>
2013-12-19 14:55:54 +0100

    Use the new plug-in subsystem for the concept mapper

--- appcomposer/application.py
+++ appcomposer/application.py
@@ -81,13 +81,16 @@ initialize_admin_component(app)
 #####
 
 from .composers.translate import translate_blueprint
-from .composers.adapt import adapt_blueprint
+from .composers.adapt import adapt_blueprint, adaptors_blueprints, load_plugins
 from .composers.expert import expert_blueprint
 from .composers.dummy import dummy_blueprint
 
 
 app.register_blueprint(translate_blueprint, url_prefix='/composers/translate')
 app.register_blueprint(adapt_blueprint, url_prefix='/composers/adapt')
+load_plugins()
+for adaptor_blueprint in adaptors_blueprints:
+    app.register_blueprint(adaptor_blueprint)
 app.register_blueprint(expert_blueprint, url_prefix='/composers/expert')
 app.register_blueprint(dummy_blueprint, url_prefix=dummy_info["url"])
 
--- golab_adapt_concept_mapper/__init__.py
+++ golab_adapt_concept_mapper/__init__.py
@@ -1,47 +1,39 @@
 import json
 from collections import OrderedDict
-from flask import render_template, request, flash
+from flask import render_template, request, flash, url_for
 import appcomposer.appstorage.api as appstorage
 
-from appcomposer.composers.adapt import adapt_blueprint
+from appcomposer.composers.adapt import create_adaptor
+adaptor = create_adaptor('Concept Mapper', lambda : {
+        'concepts' : ''
+   })
 
-def concept_map_load(app, app_id, name, data):
-    # If the app data is empty (basic JSON schema), we are editing a new app. Otherwise, the data values are loaded from the database.
-    if len(data) == 4:
-        return render_template("composers/adapt/conceptmapper/edit.html", app=app, app_id = app_id, name = name, n_rows = 0)
 
-    concepts = data["concepts"]
-    return render_template("composers/adapt/conceptmapper/edit.html", app=app, app_id = app_id, name = name, concepts = concepts)
+@adaptor.edit_route
+def edit(app_id):
+    data = adaptor.load_data(app_id)
+    name = data["concepts"]
 
-def concept_map_edit(app, app_id, name, data):
-    '''
-    data = {
-        'adaptor_version': '1',
-        'name': str(name),
-        'description': str(app_description),
-        'adaptor_type': str(adaptor_type),
-        'concepts': list()}
-    '''
-    # Retrieve the list of concepts and convert it to the format supported by the app.
-    # Request-- concepts: "a,b,c"  -> Concepts  (str): "a,b,c"
+    if request.method == 'GET':
+        concepts = data["concepts"]
+        return render_template("composers/adapt/conceptmapper/edit.html", app_id = app_id, name = name, concepts = concepts)
 
-    concepts = ', '.join(list(OrderedDict.fromkeys([ s.strip() for s in request.form["concepts"].split(',') ])))
+    else:
+        # Retrieve the list of concepts and convert it to the format supported by the app.
+        # Request-- concepts: "a,b,c"  -> Concepts  (str): "a,b,c"
+        concepts = ', '.join(list(OrderedDict.fromkeys([ s.strip() for s in request.form["concepts"].split(',') ])))
 
-    # Build the JSON of the current concept map.
-    data.update({
-        "concepts": concepts})
+        # Build the JSON of the current concept map.
+        data.update({
+            "concepts": concepts})
 
-    appstorage.update_app_data(app, data)
-    flash("Concept map saved successfully", "success")
-    # flash(data["concepts"], "success")
-
-    return render_template("composers/adapt/conceptmapper/edit.html", app=app, app_id = app_id, concepts = data["concepts"])
+        adaptor.save_data(app_id, data)
+        flash("Concept map saved successfully", "success")
+        return render_template("composers/adapt/conceptmapper/edit.html", app_id = app_id, concepts = data["concepts"])
 
 
 # Auxiliar routes
-
-
-@adapt_blueprint.route("/export/<app_id>/conceptmapper/conceptmapper.html")
+@adaptor.route("/export/<app_id>/conceptmapper.html")
 def conceptmapper_index(app_id):
     """
     conceptmapper_index(app_id)
@@ -55,9 +47,9 @@ def conceptmapper_index(app_id):
     # instead of domain.js (In the original app, the "concepts" variable was stored into the index.html file)
     # The domain name is not generated here.
 
-    return render_template("composers/adapt/conceptmapper/conceptmapper.html", app_id = app_id)
+    return render_template("conceptmapper/conceptmapper.html", app_id = app_id)
 
-@adapt_blueprint.route("/export/<app_id>/conceptmapper/app.xml")
+@adaptor.route("/export/<app_id>/app.xml")
 def conceptmapper_widget(app_id):
     """
     conceptmapper_widget(app_id)
@@ -71,10 +63,10 @@ def conceptmapper_widget(app_id):
     # instead of domain.js (In the original app, the "concepts" variable was stored into the index.html file)
     # The domain name is not generated here.
 
-    return render_template("composers/adapt/conceptmapper/widget.xml", app_id = app_id)
+    return render_template("conceptmapper/widget.xml", app_id = app_id)
 
 
-@adapt_blueprint.route("/export/<app_id>/conceptmapper/domain.js")
+@adaptor.route("/export/<app_id>/domain.js")
 def conceptmapper_domain(app_id):
     """
     conceptmapper_domain(app_id)
@@ -95,20 +87,5 @@ def conceptmapper_domain(app_id):
     else:
         domain = json.dumps([ s.strip() for s in data["concepts"].split(',') ])
 
-    return render_template("composers/adapt/conceptmapper/domain.js", domain = domain)
+    return render_template("conceptmapper/domain.js", domain = domain)
 
-
-#
-# Data structure (for registering)
-#
-
-data = {
-   'version' : 1,
-   'initial' : lambda : {
-        'concepts' : ''
-   },
-   'load' : concept_map_load,
-   'edit' : concept_map_edit,
-   'name' : 'Concept Mapper',
-   'id' : 'concept_map',
-}
