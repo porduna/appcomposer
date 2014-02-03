c8e1fd13502a81c3d12d4b42c867ea3ea3d97a4c
Luis Rodriguez <luis.rodriguezgil@deusto.es>
2014-02-03 13:19:28 +0100

    [translator]: Adding lownership db funcs

--- appcomposer/composers/translate/db_helpers.py
+++ appcomposer/composers/translate/db_helpers.py
@@ -1,7 +1,54 @@
 from appcomposer import db
-from appcomposer.appstorage.api import get_app_by_name
+from appcomposer.appstorage.api import get_app_by_name, set_var
 from appcomposer.composers.translate import CFG_SAME_NAME_LIMIT
 from appcomposer.models import AppVar, App
+
+"""
+REMARKS ABOUT APPVARS FOR THE TRANSLATOR:
+
+A lownership AppVar identifies that the owner of a Spec/Language combination. It is attached
+to the App that owns it, and has "lownership" as its AppVar name, and the language code (ex:
+ca_ES) as its value.
+
+As of now, we use the term "lowner" rather than "owner" because the system is being redesigned and
+major changes are required.
+"""
+
+
+def _db_get_lowner_app(spec, lang_code):
+    """
+    Gets from the database the App that is considered the Owner for a given spec and language.
+    @param spec: String to the App's original XML.
+    @param lang_code: The language code. It should actually be a partial, lang_territory code. Only the
+    language without the territory is NOT enough.
+    @return: The owner for the App and language. None if no owner is found.
+    """
+    related_apps_ids = db.session.query(AppVar.app_id).filter_by(name="spec",
+                                                                 value=spec).subquery()
+
+    # Among those AppVars for our Spec, we try to locate a lownership AppVar for our
+    # lang code.
+    owner_app_id = db.session.query(AppVar.app_id).filter(AppVar.name == "lownership",
+                                                          AppVar.value == lang_code,
+                                                          AppVar.app_id.in_(related_apps_ids)).first()
+
+    if owner_app_id is None:
+        return None
+
+    owner_app = App.query.filter_by(id=owner_app_id[0]).first()
+    return owner_app
+
+
+def _db_declare_lownership(owner_app, lang_code):
+    """
+    Declares lownership over a given Spec and Langcode. The CALLER is responsible of ensuring
+    that no other owner for that spec and lang code exists before invoking this method.
+
+    @param owner_app: Owner App for the language.
+    @param lang_code: Language code to own.
+    @return: None.
+    """
+    set_var(owner_app, "lownership", lang_code)
 
 
 def _db_get_owner_app(spec):
