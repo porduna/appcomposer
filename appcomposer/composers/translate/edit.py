14b06ac56b8da55112bcaaa70292a20f1ab0f25d
Luis Rodriguez <luis.rodriguezgil@deusto.es>
2014-02-03 12:03:53 +0100

    [translator]: Heavy refactoring. Split views into their own files.

---
+++ appcomposer/composers/translate/edit.py
@@ -0,0 +1,88 @@
+from flask import request, render_template, flash, json, url_for, redirect
+import time
+from appcomposer.appstorage.api import get_app, update_app_data, add_var
+from appcomposer.composers.translate import translate_blueprint
+
+import backend
+
+
+@translate_blueprint.route("/edit", methods=["GET", "POST"])
+def translate_edit():
+    """ Translation editor for the selected language. """
+
+    # No matter if we are handling a GET or POST, we require these parameters.
+    appid = request.values["appid"]
+    srclang = request.values["srclang"]
+    targetlang = request.values["targetlang"]
+    srcgroup = request.values["srcgroup"]
+    targetgroup = request.values["targetgroup"]
+
+    # Retrieve the application we want to view or edit.
+    app = get_app(appid)
+    if app is None:
+        return render_template("composers/errors.html", message="App not found")
+
+    bm = backend.BundleManager.create_from_existing_app(app.data)
+    spec = bm.get_gadget_spec()
+
+    # Retrieve the bundles for our lang. For this, we build the code from the info we have.
+    srcbundle_code = backend.BundleManager.partialcode_to_fullcode(srclang, srcgroup)
+    targetbundle_code = backend.BundleManager.partialcode_to_fullcode(targetlang, targetgroup)
+
+    srcbundle = bm.get_bundle(srcbundle_code)
+    targetbundle = bm.get_bundle(targetbundle_code)
+
+    # The target bundle doesn't exist yet. We need to create it ourselves.
+    if targetbundle is None:
+        splits = targetlang.split("_")
+        if len(splits) == 2:
+            lang, country = splits
+            targetbundle = backend.Bundle(lang, country, targetgroup)
+            bm.add_bundle(targetbundle_code, targetbundle)
+
+
+    # Get the owner app.
+    owner_app = backend._db_get_owner_app(spec)
+    is_owner = owner_app == app
+
+    # This is a GET request. We are essentially viewing-only.
+    if request.method == "GET":
+        return render_template("composers/translate/edit.html", is_owner=is_owner, app=app, srcbundle=srcbundle,
+                               targetbundle=targetbundle)
+
+    # This is a POST request. We need to save the entries.
+    else:
+        # Retrieve a list of all the key-values to save. That is, the parameters which start with _message_.
+        messages = [(k[len("_message_"):], v) for (k, v) in request.values.items() if k.startswith("_message_")]
+
+        # Save all the messages we retrieved from the POST or GET params into the Bundle.
+        for identifier, msg in messages:
+            if len(msg) > 0:  # Avoid adding empty messages.
+                targetbundle.add_msg(identifier, msg)
+
+        # Now we need to save the changes into the database.
+        json_str = bm.to_json()
+        update_app_data(app, json_str)
+
+        flash("Changes have been saved", "success")
+
+        propose_to_owner = request.values.get("proposeToOwner")
+        if propose_to_owner is not None and owner_app != app:
+            # We need to propose this Bundle to the owner.
+            # Note: May be confusing: app.owner.login refers to the generic owner of the App, and not the owner
+            # we are talking about in the specific Translate composer.
+            proposalData = {"from": app.owner.login, "timestamp": time.time(), "bundle_code": targetbundle_code,
+                            "bundle_contents": targetbundle.to_jsonable()}
+            proposalJson = json.dumps(proposalData)
+
+            # Link the proposal with the Owner app.
+            add_var(owner_app, "proposal", proposalJson)
+
+            flash("Changes have been proposed to the owner")
+
+        # Check whether the user wants to exit or to continue editing.
+        if "save_exit" in request.values:
+            return redirect(url_for("user.apps.index"))
+
+        return render_template("composers/translate/edit.html", is_owner=is_owner, app=app, srcbundle=srcbundle,
+                               targetbundle=targetbundle)
---
+++ appcomposer/composers/translate/merge_existing.py
@@ -0,0 +1,58 @@
+from flask import request, redirect, url_for, json, render_template, flash
+from appcomposer.appstorage.api import get_app, update_app_data
+from appcomposer.composers.translate import translate_blueprint, backend
+from appcomposer.models import AppVar
+
+__author__ = 'lrg'
+
+
+@translate_blueprint.route("/merge_existing", methods=["GET", "POST"])
+def translate_merge_existing():
+    """
+    Provides the logic for one of the merging features. This merging method
+    was implemented before the "proposals" system, which is superior.
+    Should probably be adapted or removed in the future.
+    """
+    appid = request.values.get("appid")
+    if appid is None:
+        # An appid is required.
+        return redirect(url_for("user.apps.index"))
+    app = get_app(appid)
+
+    # If we are just viewing, we haven't chosen yet.
+    if request.method == "GET":
+
+        # Find out which is the XML of our app.
+        data = json.loads(app.data)
+        spec = data["spec"]
+
+        # Find the Apps in the DB that match our criteria. We will need direct access to the DB, at least for now.
+        appvars = AppVar.query.filter_by(name="spec", value=spec).all()
+        apps_list = [var.app for var in appvars if var.app.composer == "translate"]
+
+        return render_template('composers/translate/merge_existing.html', app=app, apps_list=apps_list)
+
+    # It is a POST. The user has just chosen an app to merge, and we should hence carry out that merge.
+    elif request.method == "POST":
+
+        # Get the App to merge from the DB
+        srcapp_id = request.values.get("srcapp")
+        if srcapp_id is None:
+            # The srcapp is required.
+            return redirect(url_for("user.apps.index"))
+        srcapp = get_app(srcapp_id)
+
+
+        # Load our own app
+        bm = backend.BundleManager.create_from_existing_app(app.data)
+
+        # Merge the srcapp into our's.
+        bm.merge_json(srcapp.data)
+
+        # Update the App's data.
+        update_app_data(app, bm.to_json())
+
+        flash("Translations merged", "success")
+
+        # Redirect so that the App is reloaded with our changes applied.
+        return redirect(url_for("translate.translate_selectlang", appid=appid))
