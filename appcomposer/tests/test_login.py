9c77c74709147a78318830d341d65ca62de2be43
Luis Rodriguez-Gil <luis.rodriguezgil@deusto.es>
2014-03-07 14:23:35 +0100

    [tests]: Login tests now use flask's test client __enter__ and __exit__ instead of with, which slightly improves readability

--- appcomposer/tests/test_login.py
+++ appcomposer/tests/test_login.py
@@ -5,6 +5,9 @@ import appcomposer.application
 
 
 class TestLogin:
+    def __init__(self):
+        self.flask_app = None
+
     def login(self, username, password, redirect=True):
         return self.flask_app.post('/login', data=dict(
             login=username,
@@ -19,45 +22,37 @@ class TestLogin:
         appcomposer.app.config['TESTING'] = True
         appcomposer.app.config['CSRF_ENABLED'] = False
         appcomposer.app.config["SECRET_KEY"] = 'secret'
+        self.flask_app = appcomposer.app.test_client()
+        self.flask_app.__enter__()
 
     def tearDown(self):
-        pass
+        self.flask_app.__exit__(None, None, None)
 
     def test_login_works(self):
-        self.flask_app = appcomposer.app.test_client()
-        with self.flask_app:
-            rv = self.login("testuser", "password")
-            assert rv.status_code == 200
-            assert "logged_in" in session
-            assert session["login"] == "testuser"
-            assert session["name"] == "Test User"
+        rv = self.login("testuser", "password")
+        assert rv.status_code == 200
+        assert "logged_in" in session
+        assert session["login"] == "testuser"
+        assert session["name"] == "Test User"
 
     def test_login_fails_when_wrong_pass(self):
-        self.flask_app = appcomposer.app.test_client()
-        with self.flask_app:
-            rv = self.login("testuser", "3413124141242142342442")
-            assert "logged_in" not in session
-            assert "login" not in session
-            assert "name" not in session
+        rv = self.login("testuser", "3413124141242142342442")
+        assert "logged_in" not in session
+        assert "login" not in session
+        assert "name" not in session
 
     def test_login_fails_when_wrong_user(self):
-        self.flask_app = appcomposer.app.test_client()
-        with self.flask_app:
-            rv = self.login("testuser23414", "21412412424")
-            assert "logged_in" not in session
-            assert "login" not in session
-            assert "name" not in session
+        rv = self.login("testuser23414", "21412412424")
+        assert "logged_in" not in session
+        assert "login" not in session
+        assert "name" not in session
 
     def test_login_redir(self):
-        self.flask_app = appcomposer.app.test_client()
-        with self.flask_app:
-            rv = self.login("testuser", "password", False)
-            assert rv.status_code == 302
-            assert rv.location == "http://localhost/user/"
+        rv = self.login("testuser", "password", False)
+        assert rv.status_code == 302
+        assert rv.location == "http://localhost/user/"
 
     def test_login_not_wrong_redir(self):
-        self.flask_app = appcomposer.app.test_client()
-        with self.flask_app:
-            rv = self.login("testuser", "wrongpassword", False)
-            assert rv.status_code != 302
-            assert rv.location != "http://localhost/user/"
+        rv = self.login("testuser", "wrongpassword", False)
+        assert rv.status_code != 302
+        assert rv.location != "http://localhost/user/"
