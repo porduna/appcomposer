b9767c68611cd836126e9f4016d1ba6aa99e72b4
mllatg <mlatorre@ieec.uned.es>
2014-03-10 23:40:20 +0100

    [config_editor] Templates

--- golab_adapt_custom_configuration/__init__.py
+++ golab_adapt_custom_configuration/__init__.py
@@ -60,6 +60,7 @@ def retrieve_url(url):
         else:
             print 'The document format is not supported'
 
+
 @adaptor.edit_route
 def edit(app_id):
     # Load data from the database for this application
@@ -77,8 +78,8 @@ def edit(app_id):
         adaptor.save_data(app_id, data)
         flash("Configuration loaded successfully", "success")
 
-    return render_template("custom_configuration/edit.html", appurl = data['appurl'],
-                debug = data['configuration']['debug'], actionlogging = data['configuration']['actionlogging'],
+    return render_template("custom_configuration/edit.html", appurl = data['appurl'], debug = data['configuration']['debug'],
+                auto_load = data['configuration']['auto_load'], actionlogging = data['configuration']['actionlogging'],
                 show_prompts = data['configuration']['show_prompts'], textarea_concepts = data['configuration']['textarea_concepts'],
                 combobox_concepts = data['configuration']['combobox_concepts'], drop_external = data['configuration']['drop_external'],
                 concepts = data['configuration']['concepts'], relations = data['configuration']['relations'])
@@ -96,17 +97,67 @@ def load_configuration(app_id):
 
     data = adaptor.load_data(app_id)
 
-    debug = json.dumps(data["debug"])
-    auto_load = json.dumps(data["auto_load"])
-    actionlogging = json.dumps(data["actionlogging"])
-    show_prompts = json.dumps(data["show_prompts"])
-    textarea_concepts = json.dumps(data["textarea_concepts"])
-    combobox_concepts = json.dumps(data["combobox_concepts"])
-    drop_external = json.dumps(data["drop_external"])
+    debug = json.dumps(data['configuration']["debug"])
+    auto_load = json.dumps(data['configuration']["auto_load"])
+    actionlogging = json.dumps(data['configuration']["actionlogging"])
+    show_prompts = json.dumps(data['configuration']["show_prompts"])
+    textarea_concepts = json.dumps(data['configuration']["textarea_concepts"])
+    combobox_concepts = json.dumps(data['configuration']["combobox_concepts"])
+    drop_external = json.dumps(data['configuration']["drop_external"])
 
-    concepts = json.dumps([ s.strip() for s in data["concepts"].split(',') ])
-    relations = json.dumps([ s.strip() for s in data["relations"].split(',') ])
+    concepts = json.dumps([ s.strip() for s in data['configuration']["concepts"].split(',') ])
+    relations = json.dumps([ s.strip() for s in data['configuration']["relations"].split(',') ])
 
-    return render_template("custom_configuration/configuration_default.js", debug = debug, actionlogging = actionlogging, show_prompts = show_prompts,
-                                            textarea_concepts = textarea_concepts, combobox_concepts = combobox_concepts, drop_external = drop_external,
-                                            concepts = concepts, relations = relations)
+    return render_template("custom_configuration/configuration_default.js", debug = debug, auto_load = auto_load, actionlogging = actionlogging,
+                                            show_prompts = show_prompts, textarea_concepts = textarea_concepts, combobox_concepts = combobox_concepts,
+                                            drop_external = drop_external, concepts = concepts, relations = relations)
+
+
+    #CLEAN THIS CODE!!!!!!!!!!!!! THINK FOR DATA.VALUES IN VIEW  
+"""
+import re
+import json
+import urllib
+import urlparse
+
+def extract_base_url(url):
+    parsed = urlparse.urlparse(url)
+    new_path = parsed.path
+    # Go to the last directory
+    if '/' in new_path:
+        new_path = new_path[:new_path.rfind('/')+1]
+    messages_file_parsed = urlparse.ParseResult(scheme = parsed.scheme, netloc = parsed.netloc, path = new_path, params = '', query = '', fragment = '')
+    return messages_file_parsed.geturl()
+
+uno = extract_base_url('http://www.google.es/tarea/ruta.html')
+print uno
+"""
+
+def parse_url(url):
+    """
+    Parses the contents from a URL.
+    @param url: URL to open.
+    @return: Contents of the response.
+    """
+    parsed = urlparse.urlparse(url)
+    domain = parsed.netloc
+    return domain
+
+
+def extract_base_url(url):
+    parsed = urlparse.urlparse(url)
+    new_path = parsed.path
+    # Go to the last directory
+    if '/' in new_path:
+        new_path = new_path[:new_path.rfind('/')+1]
+    messages_file_parsed = urlparse.ParseResult(scheme = parsed.scheme, netloc = parsed.netloc, path = new_path, params = '', query = '', fragment = '')
+    return messages_file_parsed.geturl()
+    
+
+def make_url_absolute(relative_path, url):
+    if relative_path.startswith(('http://', 'https://')):
+        return relative_path
+    return _extract_base_url(url) + relative_path
+
+
+
