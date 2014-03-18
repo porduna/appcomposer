3f8859832726e9ef917109460f900d40ebebce91
Luis Rodriguez-Gil <luis.rodriguezgil@deusto.es>
2014-03-18 12:09:14 +0100

    [translate]: Autoaccept setting and retrieval now supported through AJAX (and added tests for it)

--- appcomposer/composers/translate/ajax.py
+++ appcomposer/composers/translate/ajax.py
@@ -1,8 +1,63 @@
 from flask import request, jsonify, json
+from appcomposer.appstorage.api import get_app, update_app_data
 from appcomposer.composers.translate import translate_blueprint
 from appcomposer.composers.translate.bundles import BundleManager
 from appcomposer.composers.translate.db_helpers import _db_get_ownerships
 from appcomposer.models import AppVar
+
+AUTOACCEPT_DEFAULT = "1"
+
+@translate_blueprint.route("/config/autoaccept/<appid>", methods=["GET", "POST"])
+def autoaccept(appid):
+    """
+    JSON API to GET or POST whether to auto-accept all proposals for an app or not.
+    The POST request takes a "value" POST parameter.
+    Both return a JSON: { result: "success", value: "<0|1>" } when successful.
+    The 0 and 1 numbers are always strings.
+    """
+    result = {}
+
+    app = get_app(appid)
+    if app is None:
+        result["result"] = "error"
+        result["message"] = "appid not provided"
+        return jsonify(**result)
+
+    data = json.loads(app.data)
+
+    if request.method == "GET":
+        if "autoaccept" in data:
+            if data["autoaccept"]:
+                result["value"] = "1"
+            else:
+                result["value"] = "0"
+        else:
+            result["value"] = AUTOACCEPT_DEFAULT  # True by default.
+
+        result["result"] = "success"
+        return jsonify(**result)
+
+    if request.method == "POST":
+        value = request.values.get("value")
+        if value is None:
+            result["result"] = "error"
+            result["message"] = "value not provided"
+            return jsonify(**result)
+
+        if value == "0":
+            data["autoaccept"] = "0"
+        elif value == "1":
+            data["autoaccept"] = "1"
+        else:
+            result["result"] = "error"
+            result["message"] = "Value not recognized. Should be 0 or 1."
+            return jsonify(**result)
+
+        update_app_data(app, json.dumps(data))
+
+        result["result"] = "success"
+        result["value"] = value
+        return jsonify(**result)
 
 
 @translate_blueprint.route("/get_proposal", methods=["GET"])
---
+++ appcomposer/tests/test_translate_ajax.py
@@ -0,0 +1,117 @@
+import urllib
+from flask import json
+from werkzeug.utils import escape
+import appcomposer
+import appcomposer.application
+
+from appcomposer.appstorage import api, add_var
+from appcomposer.appstorage.api import get_app
+from appcomposer.composers.translate.db_helpers import _db_declare_ownership, _db_get_lang_owner_app, _db_get_ownerships, _find_unique_name_for_app, _db_get_proposals
+from appcomposer.login import current_user
+
+
+class TestTranslateAjax:
+    def __init__(self):
+        self.flask_app = None
+        self.tapp = None
+
+    def login(self, username, password):
+        return self.flask_app.post('/login', data=dict(
+            login=username,
+            password=password
+        ), follow_redirects=True)
+
+    def logout(self):
+        return self.flask_app.get('/logout', follow_redirects=True)
+
+    def _cleanup(self):
+        """
+        Does cleanup tasks in case the tests failed before.
+        Can be invoked *before* and *after* the tests.
+        """
+        app = api.get_app_by_name("UTApp")
+        if app is not None:
+            api.delete_app(app)
+
+        app = api.get_app_by_name("UTApp2")
+        if app is not None:
+            api.delete_app(app)
+
+        app = api.get_app_by_name("UTApp (1)")
+        if app is not None:
+            api.delete_app(app)
+
+    def setUp(self):
+        appcomposer.app.config['DEBUG'] = True
+        appcomposer.app.config['TESTING'] = True
+        appcomposer.app.config['CSRF_ENABLED'] = False
+        appcomposer.app.config["SECRET_KEY"] = 'secret'
+        self.flask_app = appcomposer.app.test_client()
+        self.flask_app.__enter__()
+
+        rv = self.login("testuser", "password")
+
+        # In case the test failed before, start from a clean state.
+        self._cleanup()
+
+        # Create an App for the tests.
+        self.tapp = api.create_app("UTApp", "translate", '{"spec":"http://justatest.com"}')
+
+        # Because it's a translate app it needs an spec when it is created, and that is in fact required by some of the tests.
+        api.add_var(self.tapp, "spec", "http://justatest.com")
+
+    def tearDown(self):
+        self._cleanup()
+        self.flask_app.__exit__(None, None, None)
+
+    def test_autoaccept_get(self):
+        get_url = u"/composers/translate/config/autoaccept/" + self.tapp.unique_id
+        rv = self.flask_app.get(get_url)
+
+        assert rv.status_code == 200
+
+        js = json.loads(rv.data)
+
+        assert js["result"] == "success"
+        assert js["value"] == "1"  # True by default.
+
+    def test_autoaccept_not_exist(self):
+        get_url = u"/composers/translate/config/autoaccept/" + "32124214124134124124"  # Non existing
+        rv = self.flask_app.get(get_url)
+
+        assert rv.status_code == 200
+
+        js = json.loads(rv.data)
+
+        assert js["result"] == "error"
+
+    def test_autoaccept_post(self):
+
+        # Check for 0 POST
+        url = u"/composers/translate/config/autoaccept/" + self.tapp.unique_id
+        rv = self.flask_app.post(url, data={"value": 0})
+        assert rv.status_code == 200
+        js = json.loads(rv.data)
+        assert js["result"] == "success"
+        assert js["value"] == "0"
+        app = get_app(self.tapp.unique_id)
+        appdata = json.loads(app.data)
+        assert appdata["autoaccept"] == "0"
+
+        # Check for 1 POST
+        url = u"/composers/translate/config/autoaccept/" + self.tapp.unique_id
+        rv = self.flask_app.post(url, data={"value": 1})
+        assert rv.status_code == 200
+        js = json.loads(rv.data)
+        assert js["result"] == "success"
+        assert js["value"] == "1"
+        app = get_app(self.tapp.unique_id)
+        appdata = json.loads(app.data)
+        assert appdata["autoaccept"] == "1"
+
+        # Check for INVALID POST
+        url = u"/composers/translate/config/autoaccept/" + self.tapp.unique_id
+        rv = self.flask_app.post(url, data={"value": 234124})  # Invalid value
+        assert rv.status_code == 200
+        js = json.loads(rv.data)
+        assert js["result"] == "error"
