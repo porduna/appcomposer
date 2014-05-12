67a08bd2ddbcca9609e48bea38ae983f8caaed76
Luis Rodriguez-Gil <luis.rodriguezgil@deusto.es>
2014-05-12 15:38:07 +0200

    [translate]: Added transfer ownership screen tests

--- appcomposer/tests/test_translate_transfer_ownership.py
+++ appcomposer/tests/test_translate_transfer_ownership.py
@@ -1,3 +1,4 @@
+import urllib
 import appcomposer
 from appcomposer.appstorage import api
 from appcomposer.appstorage.api import get_app_by_name
@@ -69,3 +70,70 @@ class TestTranslateTransferOwnership:
 
     def test_nothing(self):
         pass
+
+
+    def test_option_appears_on_edit(self):
+        """
+        Test that the transfer option does appear on the edit view.
+        """
+        url = u"/composers/translate/edit?appid=%s&srclang=all_ALL&editSelectedSourceButton=&targetlang=all_ALL&srcgroup=ALL&targetgroup=ALL" % (self.firstApp.unique_id)
+        rv = self.flask_app.get(url)
+        assert rv.status_code == 200
+        data = rv.data.decode("utf8")  # This bypasses an apparent utf8 FlaskClient bug.
+        assert "Transfer Ownership" in data
+        assert "/translate/transfer_ownership" in data
+
+    def test_option_doesnt_appear_if_nonowner(self):
+        """
+        Test that the transfer option does NOT appear on the edit view if we do not own the language.
+        """
+        url = u"/composers/translate/edit?appid=%s&srclang=all_ALL&editSelectedSourceButton=&targetlang=all_ALL&srcgroup=ALL&targetgroup=ALL" % (self.secondApp.unique_id)
+        rv = self.flask_app.get(url)
+        assert rv.status_code == 200
+        data = rv.data.decode("utf8")  # This bypasses an apparent utf8 FlaskClient bug.
+        assert "Transfer Ownership" not in data
+        assert "/translate/transfer_ownership" not in data
+
+    def test_transfer_ownership_screen_basic(self):
+        """
+        Test that we can load the transfer_ownership screen and that the contents are what we expect.
+        """
+        url = u"/composers/translate/transfer_ownership?appid=%s&lang=all_ALL" % (self.firstApp.unique_id)
+        rv = self.flask_app.get(url)
+        assert rv.status_code == 200
+        data = rv.data
+        print data
+        #data = rv.data.decode("utf8")  # This bypasses an apparent utf8 FlaskClient bug.
+        assert "UTApp" in data
+        assert "Original XML" in data
+        assert "Transfer" in data
+        assert "<select" in data
+        assert "Test User: UTApp2" in data
+
+    def test_transfer_ownership_post(self):
+        """
+        Tests that we can indeed transfer the ownership.
+        """
+        url = u"/composers/translate/transfer_ownership?%s" % urllib.urlencode(dict(
+            appid=self.firstApp.unique_id,
+            lang="all_ALL",
+            transfer=self.secondApp.unique_id
+            ))
+        rv = self.flask_app.post(url)
+        assert rv.status_code == 302
+
+        # Verify that the ownership has indeed been transferred and can thus be transferred back by
+        # secondApp.
+        url = u"/composers/translate/edit?appid=%s&srclang=all_ALL&editSelectedSourceButton=&targetlang=all_ALL&srcgroup=ALL&targetgroup=ALL" % (self.secondApp.unique_id)
+        rv = self.flask_app.get(url)
+        assert rv.status_code == 200
+        data = rv.data.decode("utf8")  # This bypasses an apparent utf8 FlaskClient bug.
+        assert "/translate/transfer_ownership" in data
+
+        # Verify that the original app is no longer an owner.
+        url = u"/composers/translate/edit?appid=%s&srclang=all_ALL&editSelectedSourceButton=&targetlang=all_ALL&srcgroup=ALL&targetgroup=ALL" % (self.firstApp.unique_id)
+        rv = self.flask_app.get(url)
+        assert rv.status_code == 200
+        data = rv.data.decode("utf8")  # This bypasses an apparent utf8 FlaskClient bug.
+        assert "/translate/transfer_ownership" not in data
+
