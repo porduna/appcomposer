aa2e4c648cca874bcfa5b8a0597d8a6c67a3d113
Luis Rodriguez-Gil <luis.rodriguezgil@deusto.es>
2014-05-14 13:41:42 +0200

    [translate]: Added new app-delete tests for the new functionalities

--- appcomposer/tests/test_translate_app_delete.py
+++ appcomposer/tests/test_translate_app_delete.py
@@ -3,8 +3,9 @@ import appcomposer
 import appcomposer.application
 
 from appcomposer.appstorage import api
-from appcomposer.appstorage.api import get_app_by_name
+from appcomposer.appstorage.api import get_app_by_name, get_app
 from appcomposer.composers.translate.bundles import BundleManager
+from appcomposer.composers.translate.db_helpers import _db_get_app_ownerships
 
 
 class TestTranslateAppDelete:
@@ -31,6 +32,9 @@ class TestTranslateAppDelete:
             app = api.get_app_by_name("UTApp")
             if app is not None:
                 api.delete_app(app)
+            app = api.get_app_by_name("UTApp2")
+            if app is not None:
+                api.delete_app(app)
 
     def setUp(self):
         appcomposer.app.config['DEBUG'] = True
@@ -54,7 +58,7 @@ class TestTranslateAppDelete:
             api.add_var(app, "spec", "http://justatest.com")
 
             # Test that a confirmation appears.
-            rv = self.flask_app.get("/composers/translate/delete?appid="+app.unique_id)
+            rv = self.flask_app.get("/composers/translate/delete?appid=" + app.unique_id)
             assert rv.status_code == 200
             assert "sure" in rv.data
             assert "Delete" in rv.data
@@ -66,10 +70,82 @@ class TestTranslateAppDelete:
             api.add_var(app, "spec", "http://justatest.com")
 
             # Test that cancel works.
-            rv = self.flask_app.post("/composers/translate/delete", data={"appid": app.unique_id, "cancel": "Cancel"}, follow_redirects=True)
+            rv = self.flask_app.post("/composers/translate/delete", data={"appid": app.unique_id, "cancel": "Cancel"},
+                                     follow_redirects=True)
             assert rv.status_code == 200
             assert get_app_by_name("UTApp") is not None  # Make sure it still exists.
 
-            rv = self.flask_app.post("/composers/translate/delete", data={"appid": app.unique_id, "delete": "Delete"}, follow_redirects=True)
+            rv = self.flask_app.post("/composers/translate/delete", data={"appid": app.unique_id, "delete": "Delete"},
+                                     follow_redirects=True)
             assert rv.status_code == 200
             assert get_app_by_name("UTApp") is None  # Make sure it no longer exists.
+
+    def test_nothing_suggested_when_simple(self):
+        """
+        Test that when there are no ownerships or nobody to receive the ownerships,
+        the transfer selection form is not displayed.
+        """
+        with self.flask_app:
+            rv = self.login("testuser", "password")
+            app = api.create_app("UTApp", "translate", "{'spec':'http://justatest.com'}")
+            api.add_var(app, "spec", "http://justatest.com")
+
+            # Test that a confirmation appears.
+            rv = self.flask_app.get("/composers/translate/delete?appid=" + app.unique_id)
+            assert rv.status_code == 200
+            assert "<select" not in rv.data
+            assert "Transfer all" not in rv.data
+
+    def test_ownership_transfer_appears(self):
+        """
+        Test that the transfer-all-ownerships form appears properly when it is
+        supposed to (we have ownerships and can transfer them).
+        """
+        with self.flask_app:
+            rv = self.login("testuser", "password")
+            app = api.create_app("UTApp", "translate", "{'spec':'http://justatest.com'}")
+            api.add_var(app, "spec", "http://justatest.com")
+            api.add_var(app, "ownership", "all_ALL_ALL")
+
+            app2 = api.create_app("UTApp2", "translate", "{'spec':'http://justatest.com'}")
+            api.add_var(app2, "spec", "http://justatest.com")
+            app2id = app2.unique_id
+
+            rv = self.flask_app.get("/composers/translate/delete?appid=" + app.unique_id)
+            assert "<select" in rv.data
+            assert "Transfer all" in rv.data
+            assert app2id in rv.data
+
+    def test_delete_with_ownership_transfer(self):
+        """
+        Test that we can indeed delete an application and transfer all ownerships
+        at the same time.
+        """
+        with self.flask_app:
+            rv = self.login("testuser", "password")
+            app = api.create_app("UTApp", "translate", "{'spec':'http://justatest.com'}")
+            api.add_var(app, "spec", "http://justatest.com")
+            api.add_var(app, "ownership", "all_ALL_ALL")
+
+            app2 = api.create_app("UTApp2", "translate", "{'spec':'http://justatest.com'}")
+            api.add_var(app2, "spec", "http://justatest.com")
+            app2id = app2.unique_id
+
+            # Test that cancel still works.
+            rv = self.flask_app.post("/composers/translate/delete", data={"appid": app.unique_id, "cancel": "Cancel"},
+                                     follow_redirects=True)
+            assert rv.status_code == 200
+            assert get_app_by_name("UTApp") is not None  # Make sure it still exists.
+
+
+            # Test that we can delete
+            rv = self.flask_app.post("/composers/translate/delete",
+                                     data={"appid": app.unique_id, "delete": "Delete", "transfer": app2id},
+                                     follow_redirects=True)
+            assert rv.status_code == 200
+            assert get_app_by_name("UTApp") is None  # Make sure it no longer exists.
+
+            # Check that the ownership was indeed transferred to the second app.
+            app2 = get_app(app2id)
+            ownerships = _db_get_app_ownerships(app2)
+            assert len(ownerships) == 1
