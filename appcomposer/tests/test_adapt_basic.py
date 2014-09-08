f6597fbc75df859450860393c81a5dd9c9abdc72
Luis Rodriguez-Gil <luis.rodriguezgil@deusto.es>
2014-09-08 17:18:45 +0200

    [adapt]: Added prelimminary adaptor unit test

---
+++ appcomposer/tests/test_adapt_basic.py
@@ -0,0 +1,59 @@
+import json
+import os
+import appcomposer
+import appcomposer.application
+
+from appcomposer.appstorage import api
+from appcomposer.appstorage.api import get_app_by_name
+from appcomposer.composers.translate.bundles import BundleManager
+
+
+class TestBasicAdaptApp:
+    """
+    Test the initial adapt screen.
+    """
+    def __init__(self):
+        self.flask_app = None
+        self.tapp = None
+
+    def login(self, username, password):
+        return self.flask_app.post('/login', data=dict(
+            login=username,
+            password=password
+        ), follow_redirects=True)
+
+    def logout(self):
+        return self.flask_app.get('/logout', follow_redirects=True)
+
+    def _cleanup(self):
+        """
+        Does cleanup tasks in case the tests failed before.
+        Can be invoked *before* and *after* the tests.
+        """
+        with self.flask_app:
+            pass
+
+    def setUp(self):
+        appcomposer.app.config['DEBUG'] = True
+        appcomposer.app.config['TESTING'] = True
+        appcomposer.app.config['CSRF_ENABLED'] = False
+        appcomposer.app.config["SECRET_KEY"] = 'secret'
+        self.flask_app = appcomposer.app.test_client()
+        self.flask_app.get("/")
+        rv = self.login("testuser", "password")
+
+        # In case the test failed before, start from a clean state.
+        self._cleanup()
+
+    def tearDown(self):
+        self._cleanup()
+
+    def test_adapt_index(self):
+        """
+        Ensure that the index page is what we expect.
+        """
+        # Ensure that the index page is what we expect. The page to choose the URL, etc.
+        rv = self.flask_app.get("/composers/adapt/")
+        assert rv.status_code == 200
+        print "DATA: " + rv.data
+        assert "Create" in rv.data
