a8e2d0cdd123c15ba8844fd56caa41c15ea2db62
Luis Rodriguez-Gil <luis.rodriguezgil@deusto.es>
2014-11-04 16:50:44 +0100

    [adapt]: view_preview is now optionally public

--- appcomposer/composers/adapt/view_preview.py
+++ appcomposer/composers/adapt/view_preview.py
@@ -5,20 +5,23 @@ from appcomposer.appstorage import api as appstorage
 from appcomposer.babel import gettext
 from appcomposer.composers.adapt import adapt_blueprint, ADAPTORS
 from appcomposer.composers.adapt.utils import shindig_url
-from appcomposer.login import requires_login
+from appcomposer.login import requires_login, current_user
 
 
 @adapt_blueprint.route("/preview/<appid>/", methods=['GET', 'POST'])
-@requires_login
 def adapt_preview(appid):
     """
     adapt_preview(appid)
     Previews an application. You can preview the app of any user.
     # TODO: Eventually the preview feature should probably be merged into view_edit, through a read-only mode.
+    LOGIN IS OPTIONAL FOR THIS METHOD.
     @param appid: Appid of the app to preview.
     """
     if not appid:
         return render_template("composers/errors.html", message=gettext("appid not provided")), 400
+
+    # Check whether we are logged in.
+    logged_in = current_user() is not None
 
     # TODO: Improve this: do not load the whole thing. We just need the variables.
     app = appstorage.get_app(appid)
@@ -47,4 +50,4 @@ def adapt_preview(appid):
     spec_url = appdata["url"]
 
 
-    return render_template("composers/adapt/preview.html", app_id=appid, app_url=app_url, spec_url=spec_url, name=app.name, preview_url=preview_url)
+    return render_template("composers/adapt/preview.html", logged_in=logged_in, app_id=appid, app_url=app_url, spec_url=spec_url, name=app.name, preview_url=preview_url)
