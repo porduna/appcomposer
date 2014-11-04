d3d993582310f2f8141d4a81c983343ae81adfd3
Luis Rodriguez-Gil <luis.rodriguezgil@deusto.es>
2014-11-04 13:37:23 +0100

    More migration

--- alembic/versions/561b1cc23d6_added_spec_url_to_re.py
+++ alembic/versions/561b1cc23d6_added_spec_url_to_re.py
@@ -8,6 +8,8 @@ Create Date: 2014-11-04 09:57:23.710248
 
 
 # revision identifiers, used by Alembic.
+import json
+from appcomposer.models import App
 
 revision = '561b1cc23d6'
 down_revision = '4211b3736e90'
@@ -25,6 +27,19 @@ def upgrade():
     op.execute("UPDATE Apps SET spec_url = (SELECT value FROM AppVars WHERE name = 'spec' and app_id = Apps.id)")
     op.execute("DELETE FROM AppVars WHERE name = 'spec'")
 
+    # We will also try to extract the spec from the JSON data when possible.
+    connection = op.get_bind()
+    oldapp_ids = connection.execute("SELECT id, data FROM Apps WHERE spec_url IS NULL")
+    for oldapp_id, data in oldapp_ids:
+        try:
+            data = json.loads(data)
+            spec = data["spec"]
+            op.execute(
+                App.update().where(id==op.inline_literal(oldapp_id)).values({"spec_url": op.inline_literal(spec)})
+            )
+        except:
+            print "Exception on an app: %r" % oldapp_id
+            
 def downgrade():
 
     # To downgrade we need to add an AppVar with the spec to every App.
