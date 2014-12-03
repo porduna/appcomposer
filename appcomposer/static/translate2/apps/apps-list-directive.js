792a70013461e3f3c235a09207108cb05360b57d
Luis Rodriguez-Gil <luis.rodriguezgil@deusto.es>
2014-12-03 19:01:07 +0100

    [tr2]: Started adding the service etc

--- appcomposer/static/translate2/apps/apps-list-directive.js
+++ appcomposer/static/translate2/apps/apps-list-directive.js
@@ -1,10 +1,42 @@
 angular.module("translateApp")
-    .directive("acAppsList", function(){
+    .directive("acAppsList", function ($) {
         return {
             restrict: "E",
             templateUrl: "apps/apps-list-directive.html",
-            link: function( scope, element, attrs ) {
+            link: function (scope, element, attrs) {
 
-            }
-        }
-    });
+                initializeAppSearch();
+
+                function initializeAppSearch() {
+                    appsearch = $("#appsearch-table").DataTable({
+                        language: {
+//                        "search": "{{ gettext("Search:") }}",
+//                        "processing": "{{ gettext("Processing...") }}",
+//                        "info": "{{ gettext("Showing page _PAGE_ of _PAGES_") }}",
+//                        "lengthMenu": "{{ gettext("Display _MENU_ records per page") }}",
+//                        "zeroRecords": "{{ gettext("Nothing found") }}",
+//                        "infoEmpty": "{{ gettext("No records available") }}",
+//                        "infoFiltered": "{{ gettext("(filtered from _MAX_ total records)") }}",
+//                        "paginate": {
+//                            first: "{{ gettext("First") }}",
+//                            previous: "{{ gettext("Previous") }}",
+//                            next: "{{ gettext("Next") }}",
+//                            last: "{{ gettext("Last") }}"
+//                          },
+
+                        },
+                        autoWidth: false,
+                        lengthChange: true,
+                        columnDefs: [
+                            {
+                                "targets": 2,
+                                "sortable": false,
+                                "width": "50%"
+                            }
+                        ]
+                    });
+                }
+
+            } // !link
+        }; //! return
+    }); //! directive
---
+++ appcomposer/static/translate2/apps/apps-list-service.js
@@ -0,0 +1,16 @@
+angular
+    .module("translateApp")
+    .factory("appsListService", appsListService);
+
+
+function appsListService(appsList) {
+    var service = {
+        retrieve: retrieve
+    };
+    return service;
+
+
+    function retrieve() {
+
+    }
+}
