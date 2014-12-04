482573edeb270881fd144af7c509ebefb41dab72
Luis Rodriguez-Gil <luis.rodriguezgil@deusto.es>
2014-12-04 14:14:55 +0100

    [tr2]: Options now working

--- appcomposer/static/translate2/apps/apps-ctrl.js
+++ appcomposer/static/translate2/apps/apps-ctrl.js
@@ -1,4 +1,14 @@
 angular.module("translateApp")
-    .controller("AppsCtrl", function ($scope, $resource, DTOptionsBuilder, DTColumnBuilder) {
+    .controller("AppsCtrl", function ($scope, $resource, DTOptionsBuilder, DTColumnDefBuilder) {
         $scope.apps = $resource(APP_DYN_ROOT + "translations").query();
+
+        $scope.dt = {};
+
+        $scope.dt.columnDefs = [
+            DTColumnDefBuilder.newColumnDef(0).notSortable(),
+            DTColumnDefBuilder.newColumnDef(1).notSortable(),
+            DTColumnDefBuilder.newColumnDef(2).notSortable()
+        ];
+
+        $scope.dt.options = DTOptionsBuilder.newOptions().withPaginationType('full_numbers').withDisplayLength(2);
     });
