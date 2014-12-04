7fdaf0210aba1e9fc02d5dd1a53d539ee6f70347
Luis Rodriguez-Gil <luis.rodriguezgil@deusto.es>
2014-12-04 16:17:33 +0100

    [tr2]: Basic screen layout working

--- appcomposer/static/translate2/apps/apps-ctrl.js
+++ appcomposer/static/translate2/apps/apps-ctrl.js
@@ -1,17 +1,35 @@
-angular.module("translateApp")
-    .controller("AppsCtrl", function ($scope, $resource, DTOptionsBuilder, DTColumnDefBuilder) {
-        $scope.apps = $resource(APP_DYN_ROOT + "translations").query();
+angular
+    .module("translateApp")
+    .controller("AppsCtrl", AppsCtrl);
 
-        $scope.dt = {};
 
-        $scope.dt.columnDefs = [
-            DTColumnDefBuilder.newColumnDef(0).notSortable().withOption("width", "30%"),
-            DTColumnDefBuilder.newColumnDef(1).notSortable().withOption("width", "40%"),
-            DTColumnDefBuilder.newColumnDef(2).notSortable()
-        ];
+function AppsCtrl($scope, $resource, DTOptionsBuilder, DTColumnDefBuilder) {
+    $scope.apps = $resource(APP_DYN_ROOT + "translations").query();
 
-        $scope.dt.options = DTOptionsBuilder.newOptions()
-            .withPaginationType('full_numbers')
-            .withDisplayLength(15)
-            .withOption("autoWidth", true);
-    });
+    $scope.dt = {};
+
+    $scope.dt.columnDefs = [
+        DTColumnDefBuilder.newColumnDef(0).notSortable().withOption("width", "30%"),
+        DTColumnDefBuilder.newColumnDef(1).notSortable().withOption("width", "40%"),
+        DTColumnDefBuilder.newColumnDef(2).notSortable()
+    ];
+
+    $scope.dt.options = DTOptionsBuilder.newOptions()
+        .withPaginationType('full_numbers')
+        .withDisplayLength(15)
+        .withOption("autoWidth", true);
+
+
+    $scope.completionToColor = completionToColor;
+    /**
+     * Converts a completion percent of a language into an appropriate
+     * HTML color string.
+     *
+     * @param {float} completion: Completion from 0 to 1.
+     * @return {string}: The HTML color. Fully green if 1, fully red if 0.
+     */
+    function completionToColor(completion) {
+        return "FF0000";
+    }
+
+}
