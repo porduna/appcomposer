de1fb8cdcd2257e47a194a8e702b9d7ca950e088
Luis Rodriguez-Gil <luis.rodriguezgil@deusto.es>
2014-12-16 12:22:52 +0100

    [tr2]: ui-select for langs works but doesn't look too well

--- appcomposer/ngapps/translate2/app/app/translations-tab-ctrl.js
+++ appcomposer/ngapps/translate2/app/app/translations-tab-ctrl.js
@@ -4,9 +4,31 @@ angular
 
 function TranslationsTabCtrl($scope) {
     $scope.selected = {};
+    $scope.objectKeys = objectKeys;
+    $scope.filteredObjectKeys = filteredObjectKeys;
 
     $scope.$watch("appinfo.translations", function() {
         console.log("HAI");
         $scope.selected.lang = "all_ALL";
     });
+
+    function objectKeys(map) {
+        return Object.keys(map);
+    }
+
+    function filteredObjectKeys(map, search) {
+        var keys = Object.keys(map);
+        var filteredKeys = [];
+
+        if(search.length == 0)
+            return keys;
+
+        angular.forEach(keys, function(lang, index) {
+            var text = $scope.appinfo.translations[lang].name;
+            if(text.toLowerCase().indexOf(search.toLowerCase()) != -1)
+                filteredKeys.push(lang);
+        });
+
+        return filteredKeys;
+    }
 }
