719f9411f792074b6eb3b326cb2c6a98bcad740c
Pablo Orduña <pablo.orduna@deusto.es>
2015-01-26 12:24:54 +0100

    Start new translator

--- appcomposer/composers/translate2/translation_listing.py
+++ appcomposer/composers/translate2/translation_listing.py
@@ -1,5 +1,46 @@
 import requests
 import random
+import traceback
+
+import appcomposer.db as db
+from appcomposer.models import RepositoryApp
+
+GOLAB_REPO = 'golabz'
+EXTERNAL_REPO = 'external'
+
+def download_golab_translations():
+    try:
+        apps = requests.get("http://www.golabz.eu/rest/apps/retrieve.json").json()
+    except:
+        traceback.print_exc()
+        return
+
+    apps_by_url = {}
+    for app in apps:
+        apps_by_url[app['url']] = app
+    
+    # 
+    # This requires several steps.
+    # 
+    # Step 1: synchronize with the golabz repo
+    ##########################################
+    # 
+    # Delete deprecated apps
+    # 
+    stored_apps = db.session.query(RepositoryApp).filter(repository=GOLAB_REPO).all()
+    for stored_app in stored_apps:
+        if stored_app.url not in apps_by_url:
+            print stored_app.url, "not in the golabz repo anymore. Remove visibility?"
+            # TODO
+
+    # 
+    # Add new apps
+    # 
+    for url in apps_by_url:
+        pass
+    
+
+
 
 def retrieve_translations():
     existing_apps = requests.get("http://www.golabz.eu/rest/apps/retrieve.json").json()
