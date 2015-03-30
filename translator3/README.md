ca1ca5fff64278dd814bca0ee9704710d8a12f0d
Luis Rodriguez Gil <luis.rodriguezgil@deusto.es>
2015-03-30 10:03:17 +0200

    [translator3]: Initial commit

---
+++ translator3/README.md
@@ -0,0 +1,9 @@
+# appTranslate
+
+## Build & development
+
+Run `grunt` for building and `grunt serve` for preview.
+
+## Testing
+
+Running `grunt test` will run the unit tests with karma.
---
+++ translator3/app/main/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name hwboardApp
+ * @description
+ *
+ * Main module of the application.
+ */
+angular
+    .module('hwboard', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch',
+        'ui.slider'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'main/main.html',
+                controller: 'MainController'
+            })
+            .when('/about', {
+                templateUrl: 'main/about.html',
+                controller: 'AboutController'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
