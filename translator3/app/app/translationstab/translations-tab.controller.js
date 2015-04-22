4a8caaad71b371156d5c856d472129c3faa5f2ee
Luis Rodriguez Gil <luis.rodriguezgil@deusto.es>
2015-04-22 12:50:19 +0200

    [translator]: Preview is now 100% width, fixed minor glitches, translation percent now appears on app details

--- translator3/app/app/translationstab/translations-tab.controller.js
+++ translator3/app/app/translationstab/translations-tab.controller.js
@@ -5,15 +5,26 @@ angular
 function TranslationsTabController($scope, $log) {
 
     $scope.translatedPercent = translatedPercent;
+    $scope.translatedPercentStr = translatedPercentStr;
 
 
-    function translatedPercent(selected) {
+    function translatedPercentStr(selected) {
         try {
             var data = $scope.appinfo.translations[selected].targets["ALL"];
             var ratio = data.translated / data.items;
             return "" + data.translated + " / " + data.items + " (" + (ratio * 100.0).toFixed(2) + "%)";
         } catch(err) {
             return "N/A";
+        }
+    } // !translatedPercent
+
+    function translatedPercent(selected) {
+        try {
+            var data = $scope.appinfo.translations[selected].targets["ALL"];
+            var ratio = data.translated / data.items;
+            return ratio;
+        } catch(err) {
+            return undefined;
         }
     } // !translatedPercent
 
