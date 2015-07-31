22bb8d93df1252b77a7e3250b1f7d160e9e4d268
Pablo Orduña <pablo.orduna@deusto.es>
2015-08-01 00:44:38 +0200

    Show applications

--- appcomposer/embed/__init__.py
+++ appcomposer/embed/__init__.py
@@ -1,11 +1,15 @@
+import traceback
 from flask import Blueprint, render_template
+
 from appcomposer import db
-from appcomposer.babel import gettext
+from appcomposer.babel import gettext, lazy_gettext
 from appcomposer.login import requires_golab_login, current_golab_user
 from appcomposer.models import EmbedApplication, EmbedApplicationTranslation
 
 from flask.ext.wtf import Form
 from wtforms import TextField
+from wtforms.validators import required
+from wtforms.fields.html5 import URLField
 
 embed_blueprint = Blueprint('embed', __name__)
 
@@ -28,7 +32,7 @@ def app(identifier):
     if application is None:
         return render_template("embed/error.html", message = gettext("Application '{identifier}' not found").format(identifier=identifier)), 404
 
-    return render_template("embed/app.html", app = app, title = gettext("Application {name}").format(name=application.name))
+    return render_template("embed/app.html", app = application, title = gettext("Application {name}").format(name=application.name))
 
 @embed_blueprint.route('/apps/<identifier>/app.xml')
 def app_xml(identifier):
@@ -36,7 +40,7 @@ def app_xml(identifier):
     if application is None:
         return render_template("embed/error.xml", message = gettext("Application '{identifier}' not found").format(identifier=identifier)), 404
 
-    return render_template("embed/app.html", app = app, title = gettext("Application {name}").format(name=application.name))
+    return render_template("embed/app.html", app = application, title = gettext("Application {name}").format(name=application.name))
 
 # 
 # Management URLs
@@ -49,17 +53,33 @@ def index():
     return render_template("embed/index.html", applications = applications)
 
 class ApplicationForm(Form):
-    pass
+    name = TextField(lazy_gettext("Name:"), validators=[required()])
+    url = URLField(lazy_gettext("Web:"), validators=[required()])
 
 @embed_blueprint.route('/create', methods = ['GET', 'POST'])
 @requires_golab_login
 def create():
     form = ApplicationForm()
+    if form.validate_on_submit():
+        application = EmbedApplication(url = form.url.data, name = form.name.data, owner = current_golab_user())
+        db.session.add(application)
+        try:
+            db.session.commit()
+        except Exception as e:
+            traceback.print_exc()
+            return render_template("embed/error.html", message = gettext("There was an error creating an application")), 500
+        else:
+            return redirect(url_for('.edit', identifier=application.identifier))
+            
     return render_template("embed/create.html", form=form)
 
 @embed_blueprint.route('/edit/<identifier>/', methods = ['GET', 'POST'])
 @requires_golab_login
 def edit(identifier):
-    form = ApplicationForm()
+    application = db.session.query(EmbedApplication).filter_by(identifier = identifier).first()
+    form = ApplicationForm(obj=application)
+    if form.validate_on_submit():
+        application.update(url=form.url.data, name=form.name.data)
+        db.session.commit()
     return render_template("embed/edit.html", form=form)
 
