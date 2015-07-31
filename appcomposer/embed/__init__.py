342e2237dffdfac45ec0bd6027fee86f9926f348
Pablo Orduña <pablo.orduna@deusto.es>
2015-07-31 23:20:06 +0200

    Add /embed/

--- appcomposer/embed/__init__.py
+++ appcomposer/embed/__init__.py
@@ -1,9 +1,16 @@
 from flask import Blueprint, render_template
 from appcomposer import db
 from appcomposer.babel import gettext
+from appcomposer.login import requires_golab_login, current_golab_user
 from appcomposer.models import EmbedApplication, EmbedApplicationTranslation
 
+from flask.ext.wtf import Form
+
 embed_blueprint = Blueprint('embed', __name__)
+
+@embed_blueprint.context_processor
+def inject_variables():
+    return dict(current_golab_user=current_golab_user)
 
 # 
 # Public URLs
@@ -19,6 +26,7 @@ def app(identifier):
     application = db.session.query(EmbedApplication).filter_by(identifier = identifier).first()
     if application is None:
         return render_template("embed/error.html", message = gettext("Application '{identifier}' not found").format(identifier=identifier)), 404
+
     return render_template("embed/app.html", app = app, title = gettext("Application {name}").format(name=application.name))
 
 @embed_blueprint.route('/apps/<identifier>/app.xml')
@@ -27,13 +35,30 @@ def app_xml(identifier):
     if application is None:
         return render_template("embed/error.xml", message = gettext("Application '{identifier}' not found").format(identifier=identifier)), 404
 
-    return
+    return render_template("embed/app.html", app = app, title = gettext("Application {name}").format(name=application.name))
 
 # 
 # Management URLs
 # 
 
 @embed_blueprint.route('/')
+@requires_golab_login
 def index():
-    return ":-)"
+    applications = db.session.query(EmbedApplication).filter_by(owner = current_golab_user()).order_by(EmbedApplication.last_update).all()
+    return render_template("embed/index.html", applications = applications)
+
+class ApplicationForm(Form):
+    pass
+
+@embed_blueprint.route('/create', methods = ['GET', 'POST'])
+@requires_golab_login
+def create():
+    form = ApplicationForm()
+    return render_template("embed/create.html", form=form)
+
+@embed_blueprint.route('/edit/<identifier>/', methods = ['GET', 'POST'])
+@requires_golab_login
+def edit(identifier):
+    form = ApplicationForm()
+    return render_template("embed/edit.html", form=form)
 
