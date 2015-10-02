a4475cd16872fd8d9ac234def35f7cd48eb20623
Pablo Orduña <pablo.orduna@deusto.es>
2015-10-02 05:03:13 +0200

    Show only graasp-json

--- appcomposer/templates/translator/apps_angular_js.js
+++ appcomposer/templates/translator/apps_angular_js.js
@@ -7,15 +7,21 @@ appList.controller('AppListCtrl', function ($scope) {
       url: APPS_URL,
       success: function (obj) {
         $scope.$apply(function(scope) {
+            var allApps = [];
             $.each(obj.apps, function( pos, appset ){
                 $.each(appset.apps, function (pos, app) {
-                    console.log(app);
                     var d = new Date(app.last_change.replace(/ /, 'T'));
                     app.last_change = d.getFullYear() + "-" + zfill(d.getMonth() + 1) + "-" + zfill(d.getDate()) + " " + zfill(d.getHours()) + ":" + zfill(d.getMinutes()) + ":" + zfill(d.getSeconds());
+                    allApps.push(app);
                 });
             });
-
             scope.apps = obj.apps;
+            scope.$apply();
+            if (allApps.length == 1) {
+                allApps[0].current_include = 'panelbody.html';
+                var appUrlHash = allApps[0].app_url_hash;
+                $("#collapse-" + appUrlHash).collapse('show');
+            }
         });
       },
       dataType: "json"
