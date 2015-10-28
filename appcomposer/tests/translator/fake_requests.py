c39af09aec02808be6119e137f8fa2e760cc3464
Pablo Orduña <pablo.orduna@deusto.es>
2015-10-28 23:28:41 +0100

    Start adding apps

--- appcomposer/tests/translator/fake_requests.py
+++ appcomposer/tests/translator/fake_requests.py
@@ -1,4 +1,5 @@
 import mock
+import json
 import requests
 
 def _response(contents):
@@ -6,9 +7,27 @@ def _response(contents):
     r._content = contents
     return r
 
+APPS = []
+
+for app_id in range(10):
+    APPS.append({
+        "title": "APP%s" % app_id,
+        "id": "%s" % app_id,
+        "author": "admin",
+        "description": "DESCRIPTION%s" % app_id,
+        "app_url": "http://url%s/gadget.xml" % app_id,
+        "app_type": "OpenSocial gadget",
+        "app_image": "http://www.golabz.eu/logo%s.png" % app_id,
+        "app_thumb": "http://www.golabz.eu/logo_thumb%s.png" % app_id,
+        "app_golabz_page": "http://www.golabz.eu/apps/page%s" % app_id
+    })
+
+LABS = []
+
+
 SIDE_EFFECT = {
-    "http://www.golabz.eu/rest/apps/retrieve.json": _response("[]"),
-    "http://www.golabz.eu/rest/labs/retrieve.json": _response("[]"),
+    "http://www.golabz.eu/rest/apps/retrieve.json": _response(json.dumps(APPS)),
+    "http://www.golabz.eu/rest/labs/retrieve.json": _response(json.dumps(LABS)),
 }
 
 def create_requests_mock():
