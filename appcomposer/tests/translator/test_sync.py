af4ee4cbe095d1f43e7f44fe86d04f8ea04bd3aa
Pablo Orduña <pablo.orduna@deusto.es>
2015-11-09 11:06:28 +0100

    Refactor assertions in tests to make it faster

--- appcomposer/tests/translator/test_sync.py
+++ appcomposer/tests/translator/test_sync.py
@@ -14,141 +14,97 @@ class TranslatorTest(ComposerTest):
         mongo_translation_urls.remove()
         mongo_bundles.remove()
 
-    def assertApp1(self):
-        # Check MongoDB (English and Spanish)
-        resultEngUrl = mongo_translation_urls.find_one({'_id':'en_ALL_ALL::http://url1/languages/en_ALL.xml'})
-        resultEngApp = mongo_bundles.find_one({'_id':'en_ALL_ALL::http://url1/gadget.xml'})
-        self.assertEquals(resultEngUrl['data'], resultEngApp['data'])
-        data = json.loads(resultEngUrl['data'])
-        self.assertEquals("Message1_1", data['message1_1'])
-        self.assertEquals("Message2_1", data['message2_1'])
-        self.assertEquals("Message3_1", data['message3_1'])
-        self.assertEquals("Message4_1", data['message4_1'])
+    def assertAppMongoDB(self, language, url, messages):
+        resultUrl = mongo_translation_urls.find_one({'_id':'{0}_ALL_ALL::http://{1}/languages/en_ALL.xml'.format(language, url)})
+        resultApp = mongo_bundles.find_one({'_id':'{0}_ALL_ALL::http://{1}/gadget.xml'.format(language, url)})
+        self.assertEquals(resultUrl['data'], resultApp['data'])
+        data = json.loads(resultUrl['data'])
+        self.assertEqual(json.dumps(data), json.dumps(messages))
 
-        resultSpaUrl = mongo_translation_urls.find_one({'_id':'es_ALL_ALL::http://url1/languages/en_ALL.xml'})
-        resultSpaApp = mongo_bundles.find_one({'_id':'es_ALL_ALL::http://url1/gadget.xml'})
-        self.assertEquals(resultSpaUrl['data'], resultSpaApp['data'])
-        data = json.loads(resultSpaUrl['data'])
+    def build_dict(self, identifier, number_of_messages, prefix, last = None):
+        d = {}
+        for x in range(number_of_messages):
+            d['message{0}_{1}'.format(x + 1, identifier)] = "{0}{1}_{2}".format(prefix, x + 1, identifier)
+        if last:
+            d['message{0}_{1}'.format(number_of_messages, identifier)] = "{0}{1}_{2}".format(last, number_of_messages, identifier)
+        return d
 
-        self.assertEquals("Mensaje1_1", data['message1_1'])
-        self.assertEquals("Mensaje2_1", data['message2_1'])
-        self.assertEquals("Mensaje3_1", data['message3_1'])
-        # This is self-filled by its English version
-        self.assertEquals("Message4_1", data['message4_1'])
+    def assertMessages(self, messages, expected_messages):
+        for message_key, message_value in expected_messages.items():
+            message = messages[message_key]
+            if message_value['can_edit']:
+                self.assertTrue(messages[message_key]['can_edit'])
+            else:
+                self.assertFalse(messages[message_key]['can_edit'])
 
-        request.args = {'app_url' : 'http://url1/gadget.xml'}
+            if message_value['from_default']:
+                self.assertTrue(messages[message_key]['from_default'])
+            else:
+                self.assertFalse(messages[message_key]['from_default'])
+
+            self.assertEqual(message_value['source'], message['source'])
+            self.assertEqual(message_value['target'], message['target'])
+
+    def assertApiTranslate(self, url, lang, automatic, preview, expected_messages = None):
+        request.args = {'app_url' : url}
 
         # Check API
-        english_results = api_translate('en_ALL', 'ALL').json
-        self.assertFalse(english_results['automatic'])
-        self.assertTrue(english_results['preview'])
-        self.assertEquals(english_results['url'], 'http://url1/gadget.xml')
-        message1_1 = english_results['translation']['message1_1']
-        self.assertFalse(message1_1['can_edit'])
-        self.assertFalse(message1_1['from_default'])
-        self.assertEquals("Message1_1", message1_1['source'])
-        self.assertEquals("Message1_1", message1_1['target'])
-            
+        results = api_translate('{0}_ALL'.format(lang), 'ALL').json
+        if automatic:
+            self.assertTrue(results['automatic'])
+        else:
+            self.assertFalse(results['automatic'])
+
+        if preview:
+            self.assertTrue(results['preview'])
+        else:
+            self.assertFalse(results['preview'])
+
+        self.assertEquals(results['url'], url)
+        
+        self.assertMessages(results['translation'], expected_messages)
+
+    def assertApp1(self):
+        self.assertAppMongoDB("en", "url1", self.build_dict(1, 4, "Message"))
+        self.assertAppMongoDB("es", "url1", self.build_dict(1, 4, "Mensaje", "Message"))
+
+        # Check API
+        self.assertApiTranslate('http://url1/gadget.xml', lang = 'en', automatic = False, preview = True, expected_messages = {
+            'message1_1': dict(can_edit=False, from_default=False, source='Message1_1', target='Message1_1'),
+        })
+        
         # In Spanish, the fourth message is special
-        spanish_results = api_translate('es_ALL', 'ALL').json
-        self.assertFalse(spanish_results['automatic'])
-        self.assertTrue(spanish_results['preview'])
-        self.assertEquals(spanish_results['url'], 'http://url1/gadget.xml')
-        message1_1 = spanish_results['translation']['message1_1']
-        self.assertFalse(message1_1['can_edit'])
-        self.assertFalse(message1_1['from_default'])
-        self.assertEquals("Message1_1", message1_1['source'])
-        self.assertEquals("Mensaje1_1", message1_1['target'])
-        message4_1 = spanish_results['translation']['message4_1']
-        self.assertTrue(message4_1['can_edit'])
-        self.assertTrue(message4_1['from_default'])
-        self.assertEquals("Message4_1", message4_1['source'])
-        self.assertEquals("Message4_1", message4_1['target'])
+        self.assertApiTranslate('http://url1/gadget.xml', lang = 'es', automatic = False, preview = True, expected_messages = {
+            'message1_1': dict(can_edit=False, from_default=False, source='Message1_1', target='Mensaje1_1'),
+            'message4_1': dict(can_edit=True,  from_default=True,  source='Message4_1', target='Message4_1'),
+        })
         
         # There is no translation to French, so it's automatic
-        french_results = api_translate('fr_ALL', 'ALL').json
-        self.assertTrue(french_results['automatic'])
-        self.assertTrue(french_results['preview'])
-        self.assertEquals(french_results['url'], 'http://url1/gadget.xml')
-        message1_1 = french_results['translation']['message1_1']
-        self.assertTrue(message1_1['can_edit'])
-        self.assertFalse(message1_1['from_default'])
-        self.assertEquals("Message1_1", message1_1['source'])
-        self.assertIsNone(message1_1['target'])
-        message4_1 = french_results['translation']['message4_1']
-        self.assertTrue(message4_1['can_edit'])
-        self.assertFalse(message4_1['from_default'])
-        self.assertEquals("Message4_1", message4_1['source'])
-        self.assertIsNone(message4_1['target'])
-
+        self.assertApiTranslate('http://url1/gadget.xml', lang = 'fr', automatic = True, preview = True, expected_messages = {
+            'message1_1': dict(can_edit=True, from_default=False, source='Message1_1', target=None),
+            'message4_1': dict(can_edit=True, from_default=False, source='Message4_1', target=None),
+        })
 
     def assertApp2(self):
-        # Check MongoDB (English and Spanish)
-        resultEngUrl = mongo_translation_urls.find_one({'_id':'en_ALL_ALL::http://url2/languages/en_ALL.xml'})
-        resultEngApp = mongo_bundles.find_one({'_id':'en_ALL_ALL::http://url2/gadget.xml'})
-        self.assertEquals(resultEngUrl['data'], resultEngApp['data'])
-        data = json.loads(resultEngUrl['data'])
-        self.assertEquals("NonAutomaticMessage1_2", data['message1_2'])
-        self.assertEquals("NonAutomaticMessage2_2", data['message2_2'])
-        self.assertEquals("NonAutomaticMessage3_2", data['message3_2'])
-        self.assertEquals("NonAutomaticMessage4_2", data['message4_2'])
-
-        resultSpaUrl = mongo_translation_urls.find_one({'_id':'es_ALL_ALL::http://url2/languages/en_ALL.xml'})
-        resultSpaApp = mongo_bundles.find_one({'_id':'es_ALL_ALL::http://url2/gadget.xml'})
-        self.assertEquals(resultSpaUrl['data'], resultSpaApp['data'])
-        data = json.loads(resultSpaUrl['data'])
-
-        self.assertEquals("NonAutomaticMensaje1_2", data['message1_2'])
-        self.assertEquals("NonAutomaticMensaje2_2", data['message2_2'])
-        self.assertEquals("NonAutomaticMensaje3_2", data['message3_2'])
-        # This is self-filled by its English version
-        self.assertEquals("NonAutomaticMessage4_2", data['message4_2'])
-
-        request.args = {'app_url' : 'http://url2/gadget.xml'}
+        self.assertAppMongoDB("en", "url2", self.build_dict(2, 4, "NonAutomaticMessage"))
+        self.assertAppMongoDB("es", "url2", self.build_dict(2, 4, "NonAutomaticMensaje", "NonAutomaticMessage"))
 
         # Check API
-        english_results = api_translate('en_ALL', 'ALL').json
-        self.assertFalse(english_results['automatic'])
-        self.assertFalse(english_results['preview'])
-        self.assertEquals(english_results['url'], 'http://url2/gadget.xml')
-        message1_2 = english_results['translation']['message1_2']
-        self.assertFalse(message1_2['can_edit'])
-        self.assertFalse(message1_2['from_default'])
-        self.assertEquals("NonAutomaticMessage1_2", message1_2['source'])
-        self.assertEquals("NonAutomaticMessage1_2", message1_2['target'])
+        self.assertApiTranslate('http://url2/gadget.xml', lang = 'en', automatic = False, preview = False, expected_messages = {
+            'message1_2': dict(can_edit=False, from_default=False, source='NonAutomaticMessage1_2', target='NonAutomaticMessage1_2'),
+        })
             
         # In Spanish, the fourth message is special
-        spanish_results = api_translate('es_ALL', 'ALL').json
-        self.assertFalse(spanish_results['automatic'])
-        self.assertFalse(spanish_results['preview'])
-        self.assertEquals(spanish_results['url'], 'http://url2/gadget.xml')
-        message1_2 = spanish_results['translation']['message1_2']
-        self.assertFalse(message1_2['can_edit'])
-        self.assertFalse(message1_2['from_default'])
-        self.assertEquals("NonAutomaticMessage1_2", message1_2['source'])
-        self.assertEquals("NonAutomaticMensaje1_2", message1_2['target'])
-        message4_2 = spanish_results['translation']['message4_2']
-        self.assertTrue(message4_2['can_edit'])
-        self.assertTrue(message4_2['from_default'])
-        self.assertEquals("NonAutomaticMessage4_2", message4_2['source'])
-        self.assertEquals("NonAutomaticMessage4_2", message4_2['target'])
+        self.assertApiTranslate('http://url2/gadget.xml', lang = 'es', automatic = False, preview = False, expected_messages = {
+            'message1_2': dict(can_edit=False, from_default=False, source='NonAutomaticMessage1_2', target='NonAutomaticMensaje1_2'),
+            'message4_2': dict(can_edit=True,  from_default=True,  source='NonAutomaticMessage4_2', target='NonAutomaticMessage4_2'),
+        })
         
         # There is no translation to French, so it's not automatic
-        french_results = api_translate('fr_ALL', 'ALL').json
-        self.assertFalse(french_results['automatic'])
-        self.assertFalse(french_results['preview'])
-        self.assertEquals(french_results['url'], 'http://url2/gadget.xml')
-        message1_2 = french_results['translation']['message1_2']
-        self.assertTrue(message1_2['can_edit'])
-        self.assertFalse(message1_2['from_default'])
-        self.assertEquals("NonAutomaticMessage1_2", message1_2['source'])
-        self.assertIsNone(message1_2['target'])
-        message4_2 = french_results['translation']['message4_2']
-        self.assertTrue(message4_2['can_edit'])
-        self.assertFalse(message4_2['from_default'])
-        self.assertEquals("NonAutomaticMessage4_2", message4_2['source'])
-        self.assertIsNone(message4_2['target'])
-
+        self.assertApiTranslate('http://url2/gadget.xml', lang = 'fr', automatic = False, preview = False, expected_messages = {
+            'message1_2': dict(can_edit=True,  from_default=False, source='NonAutomaticMessage1_2', target=None),
+            'message4_2': dict(can_edit=True,  from_default=False, source='NonAutomaticMessage4_2', target=None),
+        })
 
     def assertGraaspApp(self):
         resultEngUrl = mongo_translation_urls.find_one({'_id':'en_ALL_ALL::http://composer.golabz.eu/graasp_i18n/languages/en_ALL.xml'})
