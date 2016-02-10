6243aa87a7a168adbd9051b7a5e5b9c0980211de
Pablo Orduña <pablo.orduna@deusto.es>
2016-02-10 17:24:11 +0100

    Improve the algorithm

--- tools/fix_from_default.py
+++ tools/fix_from_default.py
@@ -31,17 +31,28 @@ with app.app_context():
             if active_message.value == '{0}':
                 reason(bundle, "value={0}")
                 continue
-
-            if not active_message.taken_from_default and active_message.from_developer and active_message.value == original_messages.get(active_message.key, '________this.will.never.be'):
+            
+#             if bundle.translation_url.url == 'http://go-lab.gw.utwente.nl/production/conceptmapper/build/languages/conceptmapper_en_ALL.xml' and active_message.key == 'help.helpDialog.content.conceptmapper' and bundle.language == 'es_ALL':
+#                 print "*" * 20
+#                 print "Processing {0}::{1}_{2}::{3} ({4!r}) ({5!r})".format(bundle.translation_url.url, bundle.language, bundle.target, active_message.key, active_message.value, original_messages.get(active_message.key))
+#                 # print not active_message.taken_from_default
+#                 # print active_message.from_developer
+#                 # print active_message.value == original_messages.get(active_message.key, '________this.will.never.be')
+#                 current = active_message.value
+#                 past = original_messages.get(active_message.key, '________this.will.never.be')
+#                 print current.strip() == past.strip()
+#                 print "*" * 20
+            if not active_message.taken_from_default and active_message.from_developer and active_message.value.strip() == original_messages.get(active_message.key, '________this.will.never.be').strip():
                 total += 1
                 print "Processing {0}::{1}_{2}::{3} ({4!r})".format(bundle.translation_url.url, bundle.language, bundle.target, active_message.key, active_message.value)
                 ph = active_message.history
-                new_history = TranslationMessageHistory(bundle = ph.bundle, key = ph.key, value = ph.value, user = get_golab_default_user(), datetime = datetime.datetime.utcnow(), parent_translation_id = ph.id, taken_from_default = True, same_tool = ph.same_tool, tool_id = ph.tool_id, fmt = ph.fmt, position = ph.position, category = ph.category, from_developer = ph.from_developer, namespace = ph.namespace)
+                original_message = original_messages.get(active_message.key, '________this.will.never.be')
+                new_history = TranslationMessageHistory(bundle = ph.bundle, key = ph.key, value = original_message, user = get_golab_default_user(), datetime = datetime.datetime.utcnow(), parent_translation_id = ph.id, taken_from_default = True, same_tool = ph.same_tool, tool_id = ph.tool_id, fmt = ph.fmt, position = ph.position, category = ph.category, from_developer = ph.from_developer, namespace = ph.namespace)
                 db.session.add(new_history)
                 db.session.flush()
                 db.session.refresh(new_history)
                 am = active_message
-                new_active_message = ActiveTranslationMessage(bundle = bundle, key = am.key, value = am.value, history = new_history, datetime = datetime.datetime.utcnow(), taken_from_default = True, position = am.position, category = am.category, from_developer = am.from_developer, namespace = am.namespace, tool_id = am.tool_id, same_tool = am.same_tool, fmt = am.fmt)
+                new_active_message = ActiveTranslationMessage(bundle = bundle, key = am.key, value = original_message, history = new_history, datetime = datetime.datetime.utcnow(), taken_from_default = True, position = am.position, category = am.category, from_developer = am.from_developer, namespace = am.namespace, tool_id = am.tool_id, same_tool = am.same_tool, fmt = am.fmt)
                 db.session.delete(active_message)
                 db.session.add(new_active_message)
 
