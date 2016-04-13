1b4d850e8e555b155343b4407743b0dbdc4bcfe4
Luis Rodriguez-Gil <luis.rodriguezgil@deusto.es>
2016-04-13 12:29:14 +0200

    Added apps filtering support to the GUI and the logic; added underscore library; done minor refactoring; removed .editorconfig file from the repository

--- translator3/app/apps/apps.controller.js
+++ translator3/app/apps/apps.controller.js
@@ -1,276 +1,292 @@
 angular
-  .module("translateApp")
-  .controller("AppsController", AppsController);
+    .module("translateApp")
+    .controller("AppsController", AppsController);
 
 
 function AppsController($scope, $resource, $compile, $filter, $log, $timeout, DTOptionsBuilder, DTColumnDefBuilder) {
-  var vm = this;
+    var vm = this;
 
-  $scope.apps = []; // To hold the apps for the current category.
-  $scope.all_apps = $resource(APP_DYN_ROOT + "api/apps/repository").query();
-  $scope.all_apps.$promise.then(onAppsRetrievalSucceeded, onAppsRetrievalRejected);
+    $scope.apps = []; // To hold the apps for the current category.
+    $scope.all_apps = $resource(APP_DYN_ROOT + "api/apps/repository").query();
+    $scope.all_apps.$promise.then(onAppsRetrievalSucceeded, onAppsRetrievalRejected);
 
-  $scope.loadingTable = false;
+    $scope.loadingTable = false;
 
-  $scope.selected = {};
-  $scope.selected.app = undefined; // To store the selected app.
-
-  vm.dt = {};
-  vm.dt.instance = {};
-
-  $scope.status = {};
-
-  $scope.currentCategory = "";
-
-  $scope.filteringLang = "en";
-
-  vm.dt.options = DTOptionsBuilder.newOptions()
-    .withPaginationType('full_numbers')
-    .withDisplayLength(10)
-    .withOption("autoWidth", true)
-    .withOption("bRetrieve", false)
-    .withOption("language", {
-      "search": $filter("translate")("Search:"),
-      "processing": $filter("translate")("Processing..."),
-      "info": $filter("translate")("Showing page _PAGE_ of _PAGES_"),
-      "lengthMenu": $filter("translate")("Display _MENU_ records per page"),
-      "zeroRecords": $filter("translate")("Nothing found"),
-      "infoEmpty": $filter("translate")("No records available"),
-      "infoFiltered": $filter("translate")("(filtered from _MAX_ total records)"),
-      "paginate": {
-        first: $filter("translate")("First"),
-        previous: $filter("translate")("Previous"),
-        next: $filter("translate")("Next"),
-        last: $filter("translate")("Last")
-      }
-    });
-
-  vm.dt.columnDefs = [
-    DTColumnDefBuilder.newColumnDef(0).notSortable().withOption("width", "30%"),
-    DTColumnDefBuilder.newColumnDef(1).notSortable().withOption("width", "40%"),
-    DTColumnDefBuilder.newColumnDef(2).notSortable()
-  ];
-
-
-  // ------------------------------------
-  // SCOPE RELATED
-  // ------------------------------------
-
-
-  // -- METHODS --
-
-  $scope.selectApp = selectApp;
-  $scope.isSelected = isSelected;
-  $scope.extractLangName = extractLangName;
-  $scope.completionToColor = completionToColor;
-  $scope.getGradientColor = getGradientColor;
-  $scope.getBadgeTitle = getBadgeTitle;
-  $scope.selectCategory = selectCategory;
-  $scope.getLanguagesList = getLanguagesList;
-  $scope.getFilteredApps = getFilteredApps;
-
-  // -- EVENTS --
-
-  // ------------------------------------
-  // IMPLEMENTATIONS
-  // ------------------------------------
-
-
-  /**
-   * Gets the apps list, but filtered.
-   */
-  function getFilteredApps(apps) {
-    return $scope.apps;
-  } // !getFilteredApps
-
-  /**
-   * Gets the list of languages that will be used to filter the visible apps.
-   */
-  function getLanguagesList() {
-    return [
-      {'code': 'en', 'name': "English"},
-      {'code': 'es', 'name': "Spanish"},
-      {'code': 'fr', 'name': "French"}
-    ];
-  } // !getLanguagesList
-
-  /**
-   * Called to select the displayed category.
-   */
-  function selectCategory(category) {
-
-    $scope.currentCategory = category;
-
-    // Clear the current selection
     $scope.selected = {};
-    $scope.selected.app = undefined;
+    $scope.selected.app = undefined; // To store the selected app.
 
-    angular.forEach($scope.all_apps, function (val, ind) {
-      if (val.id == $scope.currentCategory) {
-        $scope.apps = val.items;
-      }
+    vm.dt = {};
+    vm.dt.instance = {};
 
-    });
+    $scope.status = {};
 
-    ////$timeout( function() {
-    ////    // Find the right apps to display.
-    //    angular.forEach($scope.all_apps, function(val, ind) {
-    //        if(val.id == $scope.currentCategory) {
-    //            console.log(val.items);
-    //            $scope.apps = val.items;
-    //        }
-    //    });
-    ////
-    //    $timeout( function() {
-    //        $scope.loadingTable = false;
-    //    }, 100 );
-    //
-    //}, 100);
+    $scope.currentCategory = "";
 
+    $scope.filteringLang = "en";
+    $scope.filteringEnabled = false;
+    $scope.languagesList = getLanguagesList();
 
-  } // !selectCategory
+    vm.dt.options = DTOptionsBuilder.newOptions()
+        .withPaginationType('full_numbers')
+        .withDisplayLength(10)
+        .withOption("autoWidth", true)
+        .withOption("bRetrieve", false)
+        .withOption("language", {
+            "search": $filter("translate")("Search:"),
+            "processing": $filter("translate")("Processing..."),
+            "info": $filter("translate")("Showing page _PAGE_ of _PAGES_"),
+            "lengthMenu": $filter("translate")("Display _MENU_ records per page"),
+            "zeroRecords": $filter("translate")("Nothing found"),
+            "infoEmpty": $filter("translate")("No records available"),
+            "infoFiltered": $filter("translate")("(filtered from _MAX_ total records)"),
+            "paginate": {
+                first: $filter("translate")("First"),
+                previous: $filter("translate")("Previous"),
+                next: $filter("translate")("Next"),
+                last: $filter("translate")("Last")
+            }
+        });
 
-  /**
-   * Called when the apps retrieval method succeeds.
-   * @param data
-   */
-  function onAppsRetrievalSucceeded(data) {
-    $log.debug("Apps Retrieval Succeeded");
-
-    //// TODO:
-    //// DEbugging only.
-    //$scope.all_apps =
-    //    [
-    //        {
-    //            id: "my_labs",
-    //            category: "My labs",
-    //            items: $scope.apps
-    //        },
-    //        {
-    //            id: "golab_labs",
-    //            category: "Go-Lab labs",
-    //            items: $scope.apps
-    //        }
-    //    ];
-
-    // Select default category.
-    selectCategory($scope.all_apps[0].id);
-
-  } // !onAppsRetrievalSucceeded
-
-  /**
-   * Called when an error occurs trying to retrieve apps.
-   */
-  function onAppsRetrievalRejected(error, err, e) {
-    $scope.status.error = {};
-    $scope.status.error.message = "Not available";
-    $scope.status.error.code = "0";
-  } // !onAppsRetrievalRejected
+    vm.dt.columnDefs = [
+        DTColumnDefBuilder.newColumnDef(0).notSortable().withOption("width", "30%"),
+        DTColumnDefBuilder.newColumnDef(1).notSortable().withOption("width", "40%"),
+        DTColumnDefBuilder.newColumnDef(2).notSortable()
+    ];
 
 
-  /**
-   * Extracts an app name. That is, extracts 'en' from 'en_ALL_ALL', for instance.
-   */
-  function extractLangName(name) {
-    return name.split("_")[0];
-  } // !extractAppName
+    // ------------------------------------
+    // SCOPE RELATED
+    // ------------------------------------
 
 
-  /**
-   * Converts a completion percent of a language into an appropriate
-   * HTML color string.
-   *
-   * @param {float} completion: Completion from 0 to 1.
-   * @return {string}: The HTML color. Fully green if 1, fully red if 0.
-   */
-  function completionToColor(completion) {
-    return "FF0000";
-  }
+    // -- METHODS --
 
-  /**
-   * Selects an app in the list.
-   * @param index: Index of the selected app.
-   * @param app: The selected app.
-   */
-  function selectApp(app, index) {
+    $scope.selectApp = selectApp;
+    $scope.isSelected = isSelected;
+    $scope.extractLangName = extractLangName;
+    $scope.completionToColor = completionToColor;
+    $scope.getGradientColor = getGradientColor;
+    $scope.getBadgeTitle = getBadgeTitle;
+    $scope.refreshCategory = refreshCategory;
+    $scope.getLanguagesList = getLanguagesList;
+    $scope.getFilteredApps = getFilteredApps;
+    $scope.onFilterChanged = onFilterChanged;
 
-    // Hide the previous selection.
-    if ($scope.selected.index !== undefined) {
-      vm.dt.instance.DataTable.row($scope.selected.index).child().hide();
+    // -- EVENTS --
+
+    // ------------------------------------
+    // IMPLEMENTATIONS
+    // ------------------------------------
+
+
+    /**
+     * Gets notified when the filter's value is changed,
+     * and will thus trigger a refresh of the apps shown.
+     */
+    function onFilterChanged() {
+        refreshCategory($scope.currentCategory);
+    } // !onFilterChanged
+
+
+    /**
+     * Gets the apps list, but filtered. If the filtering is not enabled this function will
+     * just return the apps as is.
+     *
+     * @param apps: The 'items' field from the JSON call.
+     */
+    function getFilteredApps(apps) {
+
+        if(!$scope.filteringEnabled)
+            return apps;
+
+        var ret = _.filter(apps, function (app) {
+            var filteredLang = app.languages[$scope.filteringLang.code];
+
+            // If the language is not present or if it is not 100% then we have to show this app.
+            return (filteredLang === undefined || filteredLang.progress !== 1);
+        });
+
+        return ret;
+    } // !getFilteredApps
+
+    /**
+     * Gets the list of languages that will be used to filter the visible apps.
+     */
+    function getLanguagesList() {
+        return [
+            {'id': 1, 'code': 'en', 'name': "English"},
+            {'id': 2, 'code': 'es', 'name': "Spanish"},
+            {'id': 3, 'code': 'fr', 'name': "French"},
+            {'id': 4, 'code': 'de', 'name': "German"}
+        ];
+    } // !getLanguagesList
+
+    /**
+     * Called to select the displayed category. If necessary, this will also
+     * apply any filter over the apps that is set, and it will refresh the shown
+     * apps.
+     */
+    function refreshCategory(category) {
+
+        $scope.currentCategory = category;
+
+        // Clear the current selection
+        $scope.selected = {};
+        $scope.selected.app = undefined;
+
+        angular.forEach($scope.all_apps, function (val, ind) {
+
+            if (val.id == $scope.currentCategory) {
+
+                // Apply the filter (if we must).
+                $scope.apps = getFilteredApps(val.items);
+            }
+        });
+
+
+    } // !refreshCategory
+
+    /**
+     * Called when the apps retrieval method succeeds.
+     * @param data
+     */
+    function onAppsRetrievalSucceeded(data) {
+        $log.debug("Apps Retrieval Succeeded");
+
+        //// TODO:
+        //// DEbugging only.
+        //$scope.all_apps =
+        //    [
+        //        {
+        //            id: "my_labs",
+        //            category: "My labs",
+        //            items: $scope.apps
+        //        },
+        //        {
+        //            id: "golab_labs",
+        //            category: "Go-Lab labs",
+        //            items: $scope.apps
+        //        }
+        //    ];
+
+        // Select default category.
+        refreshCategory($scope.all_apps[0].id);
+
+    } // !onAppsRetrievalSucceeded
+
+    /**
+     * Called when an error occurs trying to retrieve apps.
+     */
+    function onAppsRetrievalRejected(error, err, e) {
+        $scope.status.error = {};
+        $scope.status.error.message = "Not available";
+        $scope.status.error.code = "0";
+    } // !onAppsRetrievalRejected
+
+
+    /**
+     * Extracts an app name. That is, extracts 'en' from 'en_ALL_ALL', for instance.
+     */
+    function extractLangName(name) {
+        return name.split("_")[0];
+    } // !extractAppName
+
+
+    /**
+     * Converts a completion percent of a language into an appropriate
+     * HTML color string.
+     *
+     * @param {float} completion: Completion from 0 to 1.
+     * @return {string}: The HTML color. Fully green if 1, fully red if 0.
+     */
+    function completionToColor(completion) {
+        return "FF0000";
     }
 
-    // If we have re-selected the current selection, it is no longer
-    // selected.
-    if ($scope.selected.index == index) {
-      $scope.selected.index = undefined;
-      $scope.selected.app = undefined;
-      return;
+    /**
+     * Selects an app in the list.
+     * @param index: Index of the selected app.
+     * @param app: The selected app.
+     */
+    function selectApp(app, index) {
+
+        // Hide the previous selection.
+        if ($scope.selected.index !== undefined) {
+            vm.dt.instance.DataTable.row($scope.selected.index).child().hide();
+        }
+
+        // If we have re-selected the current selection, it is no longer
+        // selected.
+        if ($scope.selected.index == index) {
+            $scope.selected.index = undefined;
+            $scope.selected.app = undefined;
+            return;
+        }
+
+        $scope.selected.app = app;
+        $scope.selected.index = index;
+
+        if (vm.dt.instance.DataTable != undefined) {
+            var row = vm.dt.instance.DataTable.row(index);
+            var c = row.child($compile("<ac-app-details class='my-disabled-hover' app=selected.app></ac-app-details>")($scope));
+            c.show();
+        }
     }
 
-    $scope.selected.app = app;
-    $scope.selected.index = index;
+    /**
+     * Checks if the app is selected.
+     * @param app
+     */
+    function isSelected(app) {
+        if ($scope.selected.app == undefined)
+            return false;
 
-    if (vm.dt.instance.DataTable != undefined) {
-      var row = vm.dt.instance.DataTable.row(index);
-      var c = row.child($compile("<ac-app-details class='my-disabled-hover' app=selected.app></ac-app-details>")($scope));
-      c.show();
+        var result = app.title === $scope.selected.app.title;
+        return result;
     }
-  }
 
-  /**
-   * Checks if the app is selected.
-   * @param app
-   */
-  function isSelected(app) {
-    if ($scope.selected.app == undefined)
-      return false;
+    /**
+     * Method from stackoverflow to get a green to red gradient
+     * http://stackoverflow.com/a/7128796
+     * @param percent
+     */
+    function getGradientColor(percent) {
+        var percentColors = [
+            {percent: 0.0, color: {r: 0xff, g: 0x00, b: 0}},
+            {percent: 0.5, color: {r: 0xff, g: 0xff, b: 0}},
+            {percent: 1.0, color: {r: 0x00, g: 0xff, b: 0}}];
 
-    var result = app.title === $scope.selected.app.title;
-    return result;
-  }
+        for (var i = 1; i < percentColors.length - 1; i++) {
+            if (percent < percentColors[i].percent) {
+                break;
+            }
+        }
+        var lower = percentColors[i - 1];
+        var upper = percentColors[i];
+        var range = upper.percent - lower.percent;
+        var rangePct = (percent - lower.percent) / range;
+        var pctLower = 1 - rangePct;
+        var pctUpper = rangePct;
+        var color = {
+            r: Math.floor(lower.color.r * pctLower + upper.color.r * pctUpper),
+            g: Math.floor(lower.color.g * pctLower + upper.color.g * pctUpper),
+            b: Math.floor(lower.color.b * pctLower + upper.color.b * pctUpper)
+        };
+        var result = 'rgb(' + [color.r, color.g, color.b].join(',') + ')';
+        return result;
+    } // !getGradientColor
 
-  /**
-   * Method from stackoverflow to get a green to red gradient
-   * http://stackoverflow.com/a/7128796
-   * @param percent
-   */
-  function getGradientColor(percent) {
-    var percentColors = [
-      {percent: 0.0, color: {r: 0xff, g: 0x00, b: 0}},
-      {percent: 0.5, color: {r: 0xff, g: 0xff, b: 0}},
-      {percent: 1.0, color: {r: 0x00, g: 0xff, b: 0}}];
+    /**
+     * Gets the title for the specified badge.
+     */
+    function getBadgeTitle(langname, lang) {
+        var progress = sprintf("Translation progress: %s", $filter("percentage")(lang.progress, 0));
 
-    for (var i = 1; i < percentColors.length - 1; i++) {
-      if (percent < percentColors[i].percent) {
-        break;
-      }
-    }
-    var lower = percentColors[i - 1];
-    var upper = percentColors[i];
-    var range = upper.percent - lower.percent;
-    var rangePct = (percent - lower.percent) / range;
-    var pctLower = 1 - rangePct;
-    var pctUpper = rangePct;
-    var color = {
-      r: Math.floor(lower.color.r * pctLower + upper.color.r * pctUpper),
-      g: Math.floor(lower.color.g * pctLower + upper.color.g * pctUpper),
-      b: Math.floor(lower.color.b * pctLower + upper.color.b * pctUpper)
-    };
-    var result = 'rgb(' + [color.r, color.g, color.b].join(',') + ')';
-    return result;
-  } // !getGradientColor
-
-  /**
-   * Gets the title for the specified badge.
-   */
-  function getBadgeTitle(langname, lang) {
-    var progress = sprintf("Translation progress: %s", $filter("percentage")(lang.progress, 0));
-
-    if (lang.original) {
-      return "This language is provided by the original developer and your adaptation will not be applied automatically." + progress;
-    } else {
-      return "" + progress;
-    }
-  } // !getBadgeTitle
+        if (lang.original) {
+            return "This language is provided by the original developer and your adaptation will not be applied automatically." + progress;
+        } else {
+            return "" + progress;
+        }
+    } // !getBadgeTitle
 
 } // !AppsController
