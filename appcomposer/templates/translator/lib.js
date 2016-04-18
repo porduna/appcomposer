1a4de51617b153758d9db5cbc16f8490ebb0b335
Pablo Orduña <pablo.orduna@deusto.es>
2016-04-18 16:29:37 +0200

    Add $(document)

--- appcomposer/templates/translator/lib.js
+++ appcomposer/templates/translator/lib.js
@@ -1,8 +1,10 @@
 // Application found and translatable!
-$web_link = $(".field-name-field-app-web-link");
+$(document).ready(function () {
+    $web_link = $(".field-name-field-app-web-link");
 
-$("<div class=\"field field-type-link-field field-label-inline clearfix\"><div class=\"field-label\">Available languages:&nbsp;</div><div class=\"field-items\"><div class=\"field-item even\">{{ translations }} &nbsp;</div></div></div>").insertAfter($web_link);
+    $("<div class=\"field field-type-link-field field-label-inline clearfix\"><div class=\"field-label\">Available languages:&nbsp;</div><div class=\"field-items\"><div class=\"field-item even\">{{ translations }} &nbsp;</div></div></div>").insertAfter($web_link);
 
-$web_link = $(".field-name-field-weblink");
+    $web_link = $(".field-name-field-weblink");
 
-$("<div class=\"field field-type-link-field field-label-inline clearfix\"><div class=\"field-label\">Available languages:&nbsp;</div><div class=\"field-items\"><div class=\"field-item even\">{{ translations }} &nbsp;</div></div></div>").insertAfter($web_link);
+    $("<div class=\"field field-type-link-field field-label-inline clearfix\"><div class=\"field-label\">Available languages:&nbsp;</div><div class=\"field-items\"><div class=\"field-item even\">{{ translations }} &nbsp;</div></div></div>").insertAfter($web_link);
+});
