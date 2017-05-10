904048485eee2efd5b46e01dfb44e60a5782d0da
Pablo Orduña <pablo.orduna@deusto.es>
2017-05-10 18:43:12 -0500

    Move everything used in old_adapt_jsconfig to a single module

--- appcomposer/old_adapt_jsconfig.py
+++ appcomposer/old_adapt_jsconfig.py
@@ -1,14 +1,60 @@
 import re
 import json
+import pprint
 import traceback
+import urlparse
 from xml.dom import minidom
 
 from flask import render_template, url_for, Response, Blueprint
 
 from appcomposer import db
 from appcomposer.models import App
-from appcomposer.utils import inject_absolute_urls, inject_original_url_in_xmldoc, inject_absolute_locales_in_xmldoc
 from appcomposer.translator.utils import get_cached_session
+
+def extract_base_url(url):
+    parsed = urlparse.urlparse(url)
+    new_path = parsed.path
+    # Go to the last directory
+    if '/' in new_path:
+        new_path = new_path[:new_path.rfind('/')+1]
+    messages_file_parsed = urlparse.ParseResult(scheme = parsed.scheme, netloc = parsed.netloc, path = new_path, params = '', query = '', fragment = '')
+    return messages_file_parsed.geturl()
+
+def make_url_absolute(relative_path, url):
+    if relative_path.startswith(('http://', 'https://')):
+        return relative_path
+    return extract_base_url(url) + relative_path
+
+SRC_REGEXP = re.compile(r"""(<\s*(?!ng-[^<]*)[^<]*\s(src|href)\s*=\s*"?'?)(?!http://|https://|#|"|"#|'|'#| )""")
+
+def inject_absolute_urls(output_xml, url):
+    base_url = extract_base_url(url)
+    return SRC_REGEXP.sub(r"\1%s" % base_url, output_xml)
+
+def inject_original_url_in_xmldoc(xmldoc, url):
+    contents = xmldoc.getElementsByTagName("Content")
+    original_url_node = xmldoc.createElement("AppComposer")
+    original_url_node.setAttribute("originalUrl", url)
+
+    for content in contents:
+        text_node = xmldoc.createCDATASection("""
+        <script>
+            if (typeof gadgets !== "undefined" && gadgets !== null) {
+                gadgets.util.getUrlParameters().url = "%s";
+            }
+        </script>
+        """ % url)
+        content.insertBefore(text_node, content.firstChild)
+        content.parentNode.insertBefore(original_url_node, content)
+
+def inject_absolute_locales_in_xmldoc(xmldoc, url):
+    locales = xmldoc.getElementsByTagName("Locale")
+    for loc in locales:
+        messages_url = loc.getAttribute("messages")
+        new_messages_url = make_url_absolute(messages_url, url)
+        if new_messages_url != messages_url:
+            loc.setAttribute("messages", new_messages_url)
+
 
 old_adapt_jsconfig = Blueprint(__name__, 'adapt')
 
