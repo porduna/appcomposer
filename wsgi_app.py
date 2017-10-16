a277c4ecaa006072299e3a20d7c1c0047ef6c1ee
Pablo Orduña <pablo.orduna@deusto.es>
2017-10-15 22:02:03 -0700

    wsgi app

---
+++ wsgi_app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+APPCOMPOSER_DIR = os.path.dirname(__file__)
+if APPCOMPOSER_DIR == '':
+    APPCOMPOSER_DIR = os.path.abspath('.')
+
+sys.path.insert(0, APPCOMPOSER_DIR)
+os.chdir(APPCOMPOSER_DIR)
+
+sys.stdout = open('stdout.txt', 'w', 0)
+sys.stderr = open('stderr.txt', 'w', 0)
+
+import config
+
+from appcomposer import app as application
+application.config.from_object('config')
+
+import logging
+file_handler = logging.FileHandler(filename='errors.log')
+file_handler.setLevel(logging.INFO)
+application.logger.addHandler(file_handler)
