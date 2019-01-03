138162039e14c5c78415d3eb4692cb4098b59ce6
Pablo Orduña <pablo@labsland.com>
2019-01-03 00:16:28 -0800

    Increase trials

--- appcomposer/__init__.py
+++ appcomposer/__init__.py
@@ -9,7 +9,8 @@ from flask_redis import FlaskRedis
 
 redis_store = FlaskRedis()
 
-rlock = redlock.Redlock([{"host": "localhost", "port": 6379, "db": 0}, ])
+# 25 times x 200 ms = 5 seconds trying to acquire, 200 millis each time
+rlock = redlock.Redlock([{"host": "localhost", "port": 6379, "db": 0}, ], retry_count=25)
 
 from .application import app
 from .db import db, upgrader
