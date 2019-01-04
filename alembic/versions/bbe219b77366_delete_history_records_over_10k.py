2cd62d9faa3d9b4923623a7998cc5bde4b52a071
Pablo Orduña <pablo@labsland.com>
2019-01-04 01:46:40 -0800

    Be more verbose

--- alembic/versions/bbe219b77366_delete_history_records_over_10k.py
+++ alembic/versions/bbe219b77366_delete_history_records_over_10k.py
@@ -10,6 +10,7 @@ Create Date: 2019-01-04 00:44:52.915914
 revision = 'bbe219b77366'
 down_revision = '36274f13329a'
 
+import sys
 import time
 from alembic import op
 import sqlalchemy as sa
@@ -35,6 +36,10 @@ message_history = sa.Table('TranslationMessageHistory', metadata,
     sa.Column('datetime', sa.DateTime()),
 )
 
+def dbg(message):
+    print "[{}] {}".format(time.asctime(), message)
+    sys.stdout.flush()
+
 def upgrade():
     all_bundles_stmt = sql.select([
             translation_bundles.c.id, 
@@ -49,14 +54,14 @@ def upgrade():
         bundle_id = bundle_row[translation_bundles.c.id]
         count_total = list(op.get_bind().execute(sql.select([sa.func.count(message_history.c.id)], message_history.c.bundle_id == bundle_id)))[0][0]
         if count_total > 10000: # Only focus on the ones with more than 10000
-            print "Big bundle. Analyzing"
-            print " - Data:", bundle_row, count_total
+            dbg("Big bundle. Analyzing")
+            dbg(" - Data: {} {}".format(bundle_row, count_total))
             keys = [ key for key, in op.get_bind().execute(sql.select([sa.func.distinct(message_history.c.key)], message_history.c.bundle_id == bundle_id)) ]
-            print " - Keys: {}".format(len(keys))
+            dbg(" - Keys: {}".format(len(keys)))
             for key in keys:
                 records_found = list(op.get_bind().execute(sql.select([sa.func.count(message_history.c.id)], sa.and_(message_history.c.key == unicode(key), message_history.c.bundle_id == bundle_id))))[0][0]
                 if records_found > N:
-                    print "   - Key {} has {} records".format(key, records_found)
+                    dbg("   - Key {} has {} records".format(key, records_found))
 
                     while records_found > N:
                         cut_in = records_found - N
@@ -66,7 +71,7 @@ def upgrade():
                         records_found = records_found - cut_in
                         ids_to_remove_stmt = sql.select([message_history.c.id, message_history.c.datetime], sa.and_(message_history.c.key == unicode(key), message_history.c.bundle_id == bundle_id)).order_by(sa.asc(message_history.c.datetime)).limit(cut_in)
                         ids_to_remove = [ id_to_remove for id_to_remove, record_datetime in op.get_bind().execute(ids_to_remove_stmt) ]
-                        print "     - Deleting {} of them... after: {}".format(cut_in, records_found)
+                        dbg("     - Deleting {} of them... after: {}".format(cut_in, records_found))
                         op.get_bind().execute(message_history.delete(message_history.c.id.in_(ids_to_remove)))
 
 
